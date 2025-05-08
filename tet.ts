216547a9ee471d1447f4ebe0196afe9780dd2448
vinithya <vinithyareddyp.com>
2025-05-08 19:55:52 -0400

    22

--- tet.ts
+++ tet.ts
@@ -19,3 +19,12 @@ this.wpa_res_selectedItems = this.wpaResItemList.filter(item => wpaResponsibleVP
 
 const vpFilters = this.BudgetGlance_Service.facetFilter.filter(f => f.category === 'VPU Group');
 this.selectedItemsvpugroup = this.vpuItemList.filter(item => vpFilters.some(f => f.value === item.itemName));
+
+
+i see the exact issue now, the first picture shows number of filters, then it shows 13 filters ucrrently are there by default
+
+second picture - selected few options from filters-requesting VPU group. then count came to 16
+
+third picture - when i opened filters again, filter are there as 16 still shows but the boxes are unchecked 
+
+and this is happening for few filters like as in first picture, ofr budget class and business proceesess filters, it is working fine. for posting period, requesting vpu group, and responsible vpu group this issue is there, others i did not check yet 
