e568ead8f730eaca4b88d51b8704173f80a7463c
vinithya <vinithyareddyp.com>
2025-05-08 19:40:31 -0400

    18

--- tet.ts
+++ tet.ts
@@ -1,6 +1,6 @@
-if (
-  resData.title?.trim().toLowerCase() === element2.category?.trim().toLowerCase() &&
-  element[child.measureQuery.code]?.toLowerCase?.() === element2.name?.toLowerCase?.()
-) {
-  element.checked = true;
-}
+setTimeout(() => {
+  BudgetrefinerOperation.applyvpufilter(this.AllconstReqData, this.cost_obj_req_selectedItems, this.filterjsonData, this.FilterData, this.wpavpugroupData);
+  BudgetrefinerOperation.applyvpufilter(this.AllconstResData, this.cost_obj_res_selectedItems, this.filterjsonData, this.FilterData, this.wpavpugroupData);
+  BudgetrefinerOperation.applyvpufilter(this.Allwpareqdata, this.wpa_req_selectedItems, this.filterjsonData, this.FilterData, this.wpavpugroupData);
+  BudgetrefinerOperation.applyvpufilter(this.Allwparesdata, this.wpa_res_selectedItems, this.filterjsonData, this.FilterData, this.wpavpugroupData);
+}, 300);
