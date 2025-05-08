6bfbb0bca86e65c477daf78f020f4a2a7051c483
vinithya <vinithyareddyp.com>
2025-05-08 19:45:58 -0400

    19

--- tet.ts
+++ tet.ts
@@ -1,6 +1,20 @@
-setTimeout(() => {
-  BudgetrefinerOperation.applyvpufilter(this.AllconstReqData, this.cost_obj_req_selectedItems, this.filterjsonData, this.FilterData, this.wpavpugroupData);
-  BudgetrefinerOperation.applyvpufilter(this.AllconstResData, this.cost_obj_res_selectedItems, this.filterjsonData, this.FilterData, this.wpavpugroupData);
-  BudgetrefinerOperation.applyvpufilter(this.Allwpareqdata, this.wpa_req_selectedItems, this.filterjsonData, this.FilterData, this.wpavpugroupData);
-  BudgetrefinerOperation.applyvpufilter(this.Allwparesdata, this.wpa_res_selectedItems, this.filterjsonData, this.FilterData, this.wpavpugroupData);
-}, 300);
+// 🔁 Restore multiselect selections from facetFilter
+this.selectedItemsvpugroup = this.vpuItemList.filter(item =>
+  this.FilterData.some(filter => filter.category === 'VPU Group' && filter.value === item.itemName)
+);
+
+this.cost_obj_req_selectedItems = this.costObjReqItemList.filter(item =>
+  this.FilterData.some(filter => filter.category === 'Requesting VPU Group' && filter.value === item.itemName)
+);
+
+this.cost_obj_res_selectedItems = this.costObjResItemList.filter(item =>
+  this.FilterData.some(filter => filter.category === 'Responsible VPU Group' && filter.value === item.itemName)
+);
+
+this.wpa_req_selectedItems = this.wpaReqItemList.filter(item =>
+  this.FilterData.some(filter => filter.category === 'WPA Requesting VPU Group' && filter.value === item.itemName)
+);
+
+this.wpa_res_selectedItems = this.wpaResItemList.filter(item =>
+  this.FilterData.some(filter => filter.category === 'WPA Responsible VPU Group' && filter.value === item.itemName)
+);
