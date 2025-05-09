a9d5541d8fd9f1aad49c0e5d6149848e0bd0afa7
vinithya <vinithyareddyp.com>
2025-05-09 15:02:23 -0400

    45

--- action.ts
+++ action.ts
@@ -1,44 +1,17 @@
-syncCheckedState(): void {
-  const selectedFilters = this.BudgetGlance_Service.facetFilter;
-  if (!this.filterjsonData || !selectedFilters) return;
-
-  this.filterjsonData.forEach(category => {
-    category.children.forEach(child => {
-      // Level 1 options
-      child.data?.forEach(option => {
-        const match = selectedFilters.find(f =>
-          f.id === child.key &&
-          f.value?.toLowerCase() === option[child.measureQuery.code]?.toLowerCase()
-        );
-        option.checked = !!match;
-      });
-
-      // Level 2 (vpudata)
-      child.vpudata?.forEach(vpu => {
-        const match = selectedFilters.find(f =>
-          f.id === vpu.id &&
-          f.value?.toLowerCase() === vpu[vpu.childfacet]?.toLowerCase()
-        );
-        vpu.checked = !!match;
-
-        // Level 3 (deptgrpdata)
-        vpu.deptgrpdata?.forEach(deptgrp => {
-          const match2 = selectedFilters.find(f =>
-            f.id === deptgrp.id &&
-            f.value?.toLowerCase() === deptgrp[deptgrp.childfacet]?.toLowerCase()
-          );
-          deptgrp.checked = !!match2;
-
-          // Level 4 (deptdata)
-          deptgrp.deptdata?.forEach(dept => {
-            const match3 = selectedFilters.find(f =>
-              f.id === dept.id &&
-              f.value?.toLowerCase() === dept[dept.childfacet]?.toLowerCase()
-            );
-            dept.checked = !!match3;
-          });
-        });
-      });
-    });
+private syncCheckedStateForDropdown(list: any[], categoryName: string): void {
+  const activeFilters = this.FilterData.filter(f => f.category === categoryName);
+  list.forEach(item => {
+    const match = activeFilters.find(f => f.name === item.itemName || f.value === item.itemName);
+    if (match) {
+      item.checked = true;
+    }
   });
+}
+
+
+if (this.FilterData.length > 0) {
+  this.syncCheckedStateForDropdown(this.costObjReqItemList, 'Requesting VPU Group');
+  this.syncCheckedStateForDropdown(this.costObjResItemList, 'Responsible VPU Group');
+  this.syncCheckedStateForDropdown(this.wpaReqItemList, 'WPA Requesting VPU Group');
+  this.syncCheckedStateForDropdown(this.wpaResItemList, 'WPA Responsible VPU Group');
 }
