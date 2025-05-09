dd496e093b573fa2fd1e6ebf350012957321aaa4
vinithya <vinithyareddyp.com>
2025-05-08 20:22:32 -0400

    25

--- tet.ts
+++ tet.ts
@@ -1,8 +1,26 @@
-// ✅ Force UI to refresh selected items visually
 setTimeout(() => {
-  this.cost_obj_req_selectedItems = [...this.cost_obj_req_selectedItems];
-  this.cost_obj_res_selectedItems = [...this.cost_obj_res_selectedItems];
-  this.wpa_req_selectedItems = [...this.wpa_req_selectedItems];
-  this.wpa_res_selectedItems = [...this.wpa_res_selectedItems];
-  this.selectedItemsvpugroup = [...this.selectedItemsvpugroup];
-}, 0);
+  this.cost_obj_req_selectedItems = [...this.costObjReqItemList.filter(item =>
+    this.FilterData.some(filter => filter.category === 'Requesting VPU Group' && filter.value === item.itemName)
+  )];
+
+  this.cost_obj_res_selectedItems = [...this.costObjResItemList.filter(item =>
+    this.FilterData.some(filter => filter.category === 'Responsible VPU Group' && filter.value === item.itemName)
+  )];
+
+  this.wpa_req_selectedItems = [...this.wpaReqItemList.filter(item =>
+    this.FilterData.some(filter => filter.category === 'WPA Requesting VPU Group' && filter.value === item.itemName)
+  )];
+
+  this.wpa_res_selectedItems = [...this.wpaResItemList.filter(item =>
+    this.FilterData.some(filter => filter.category === 'WPA Responsible VPU Group' && filter.value === item.itemName)
+  )];
+
+  this.selectedItemsvpugroup = [...this.vpuItemList.filter(item =>
+    this.FilterData.some(filter => filter.category === 'VPU Group' && filter.value === item.itemName)
+  )];
+
+  // 👇 Force detection
+  this.cdRef.detectChanges();
+
+  this.BudgetGlance_Service.getChangeFilterEmitter('filterApply');
+}, 100); // Don't reduce this, let dropdown render
