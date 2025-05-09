ae11a782e6d0c9137e9be686ccd7ba9bf12f5c83
vinithya <vinithyareddyp.com>
2025-05-09 16:12:24 -0400

    49

--- action.ts
+++ action.ts
@@ -1,31 +1,40 @@
-const activeFilters = this.FilterData.filter(f => f.category === categoryName);
+private syncCheckedStateForDropdown(list: any[], categoryName: string): void {
+  const activeFilters = this.FilterData.filter(f => f.category === categoryName);
 
-list.forEach(item => {
-  const match = activeFilters.find(f => f.name === item.itemName || f.value === item.itemName);
-  if (match) {
-    item.checked = true;
+  // Reset the selected list
+  let targetArray = [];
 
-    switch (categoryName) {
-      case 'Requesting VPU Group':
-        if (!this.cost_obj_req_selectedItems.some(i => i.itemName === item.itemName)) {
-          this.cost_obj_req_selectedItems.push(item);
-        }
-        break;
-      case 'Responsible VPU Group':
-        if (!this.cost_obj_res_selectedItems.some(i => i.itemName === item.itemName)) {
-          this.cost_obj_res_selectedItems.push(item);
-        }
-        break;
-      case 'WPA Requesting VPU Group':
-        if (!this.wpa_req_selectedItems.some(i => i.itemName === item.itemName)) {
-          this.wpa_req_selectedItems.push(item);
-        }
-        break;
-      case 'WPA Responsible VPU Group':
-        if (!this.wpa_res_selectedItems.some(i => i.itemName === item.itemName)) {
-          this.wpa_res_selectedItems.push(item);
-        }
-        break;
-    }
+  switch (categoryName) {
+    case 'Requesting VPU Group':
+      this.cost_obj_req_selectedItems = [];
+      targetArray = this.cost_obj_req_selectedItems;
+      break;
+    case 'Responsible VPU Group':
+      this.cost_obj_res_selectedItems = [];
+      targetArray = this.cost_obj_res_selectedItems;
+      break;
+    case 'WPA Requesting VPU Group':
+      this.wpa_req_selectedItems = [];
+      targetArray = this.wpa_req_selectedItems;
+      break;
+    case 'WPA Responsible VPU Group':
+      this.wpa_res_selectedItems = [];
+      targetArray = this.wpa_res_selectedItems;
+      break;
   }
-});
+
+  list.forEach(item => {
+    const match = activeFilters.find(f => f.name === item.itemName || f.value === item.itemName);
+    if (match) {
+      targetArray.push(item); // 🔥 Push exact item object from dropdown list
+    }
+  });
+}
+
+
+if (this.FilterData.length > 0) {
+  setTimeout(() => {
+    this.syncAllDropdownStates();
+    this.cdRef.detectChanges();
+  }, 0);
+}
