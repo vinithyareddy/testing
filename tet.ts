8dce62798b2853280fe2dbaae8e7acdbb991477b
vinithya <vinithyareddyp.com>
2025-05-08 20:52:01 -0400

    27

--- tet.ts
+++ tet.ts
@@ -1,26 +1,3 @@
-// Restore Requesting VPU Group
-const matchedCostObjReq = this.costObjReqItemList.filter(item =>
-  requestingVPU.some(f => f.value === item.itemName)
-);
-this.cost_obj_req_selectedItems = [...matchedCostObjReq];
-
-// Restore Responsible VPU Group
-const matchedCostObjRes = this.costObjResItemList.filter(item =>
-  responsibleVPU.some(f => f.value === item.itemName)
-);
-this.cost_obj_res_selectedItems = [...matchedCostObjRes];
-
-// Restore WPA Requesting VPU Group
-const matchedWpaReq = this.wpaReqItemList.filter(item =>
-  wpaRequestingVPU.some(f => f.value === item.itemName)
-);
-this.wpa_req_selectedItems = [...matchedWpaReq];
-
-// Restore WPA Responsible VPU Group
-const matchedWpaRes = this.wpaResItemList.filter(item =>
-  wpaResponsibleVPU.some(f => f.value === item.itemName)
-);
-this.wpa_res_selectedItems = [...matchedWpaRes];
-
-// 🔁 Force change detection
-this.cdRef.detectChanges();
+if (match && !this.cost_obj_req_selectedItems.some(i => i.itemName === match.itemName)) {
+  this.cost_obj_req_selectedItems = this.cost_obj_req_selectedItems.concat(match);
+}
