c32365737d634ee063222b9c9e2acaf577450bed
vinithya <vinithyareddyp.com>
2025-05-09 11:14:34 -0400

    35

--- tet.ts
+++ tet.ts
@@ -1,4 +1,4 @@
-:host ::ng-deep #pbi-container {
+:host ::ng-deep .full-width-wrapper {
   position: relative;
   width: 100vw;
   left: 50%;
@@ -6,12 +6,13 @@
   margin-left: -50vw;
   margin-right: -50vw;
   background-color: #fff;
-  padding: 0 24px; // Optional spacing inside
-  box-sizing: border-box;
 }
 
+:host ::ng-deep #pbi-container,
 :host ::ng-deep .powerDiv {
   width: 100%;
-  height: 800px;
+  height: 798px;
   background-color: #fff;
+  padding: 0 24px; // Optional: add space inside edges
+  box-sizing: border-box;
 }
