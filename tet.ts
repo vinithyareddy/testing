b9b56e7eb7854cb0c3c4b26e56985c5f51b6edce
vinithya <vinithyareddyp.com>
2025-05-09 12:03:26 -0400

    38

--- tet.ts
+++ tet.ts
@@ -1,44 +1,8 @@
-setTimeout(() => {
-  const iframe = document.querySelector('#pbi-container iframe') as HTMLIFrameElement;
-  if (iframe && iframe.contentWindow) {
-    iframe.style.width = '100%';
-    iframe.style.minWidth = '100%';
-
-    // Try accessing internal doc if same-origin
-    try {
-      const doc = iframe.contentDocument || iframe.contentWindow.document;
-      const target = doc.querySelector('.reportVisualContainer') as HTMLElement;
-      if (target) {
-        target.style.width = '100%';
-        target.style.maxWidth = '100%';
-      }
-    } catch (e) {
-      // If cross-origin, skip silently
-    }
+// In your `embedRpt()` method, change settings:
+settings: {
+  ...,
+  layoutType: models.LayoutType.Custom,
+  customLayout: {
+    displayOption: models.DisplayOption.FitToWidth // or use `FitToPage` if better
   }
-}, 2000);
-
-
-:host ::ng-deep .powerDiv {
-  width: 100%;
-  height: 800px;
-  background-color: #fff;
-  overflow: hidden;
-  display: flex;
-  justify-content: center;
-}
-
-/* Force iframe to stretch */
-:host ::ng-deep iframe {
-  width: 100% !important;
-  min-width: 100% !important;
-  height: 100% !important;
-  border: none !important;
-}
-
-/* Optional if the embedded content (Power BI) needs stretching */
-:host ::ng-deep .reportVisualContainer,
-:host ::ng-deep .visualContainerHost {
-  width: 100% !important;
-  max-width: 100% !important;
 }
