d58db3c33994f4e57185800931ebf9ac9ad50459
vinithya <vinithyareddyp.com>
2025-05-09 16:04:39 -0400

    48

--- action.ts
+++ action.ts
@@ -1,6 +1,31 @@
-if (this.FilterData.length > 0) {
-  setTimeout(() => {
-    this.syncAllDropdownStates();
-    this.cd.detectChanges(); // 👈 ensures Angular reflects the model changes
-  }, 0);
-}
+const activeFilters = this.FilterData.filter(f => f.category === categoryName);
+
+list.forEach(item => {
+  const match = activeFilters.find(f => f.name === item.itemName || f.value === item.itemName);
+  if (match) {
+    item.checked = true;
+
+    switch (categoryName) {
+      case 'Requesting VPU Group':
+        if (!this.cost_obj_req_selectedItems.some(i => i.itemName === item.itemName)) {
+          this.cost_obj_req_selectedItems.push(item);
+        }
+        break;
+      case 'Responsible VPU Group':
+        if (!this.cost_obj_res_selectedItems.some(i => i.itemName === item.itemName)) {
+          this.cost_obj_res_selectedItems.push(item);
+        }
+        break;
+      case 'WPA Requesting VPU Group':
+        if (!this.wpa_req_selectedItems.some(i => i.itemName === item.itemName)) {
+          this.wpa_req_selectedItems.push(item);
+        }
+        break;
+      case 'WPA Responsible VPU Group':
+        if (!this.wpa_res_selectedItems.some(i => i.itemName === item.itemName)) {
+          this.wpa_res_selectedItems.push(item);
+        }
+        break;
+    }
+  }
+});
