7e34a73e3313f435957a8e947cb7f8a2979ff258
vinithya <vinithyareddyp.com>
2025-05-09 15:13:52 -0400

    46

--- action.ts
+++ action.ts
@@ -1,17 +1,36 @@
 private syncCheckedStateForDropdown(list: any[], categoryName: string): void {
   const activeFilters = this.FilterData.filter(f => f.category === categoryName);
+
   list.forEach(item => {
     const match = activeFilters.find(f => f.name === item.itemName || f.value === item.itemName);
     if (match) {
       item.checked = true;
+
+      switch (categoryName) {
+        case 'Requesting VPU Group':
+          if (!this.cost_obj_req_selectedItems.some(i => i.itemName === item.itemName)) {
+            this.cost_obj_req_selectedItems.push(item);
+          }
+          break;
+
+        case 'Responsible VPU Group':
+          if (!this.cost_obj_res_selectedItems.some(i => i.itemName === item.itemName)) {
+            this.cost_obj_res_selectedItems.push(item);
+          }
+          break;
+
+        case 'WPA Requesting VPU Group':
+          if (!this.wpa_req_selectedItems.some(i => i.itemName === item.itemName)) {
+            this.wpa_req_selectedItems.push(item);
+          }
+          break;
+
+        case 'WPA Responsible VPU Group':
+          if (!this.wpa_res_selectedItems.some(i => i.itemName === item.itemName)) {
+            this.wpa_res_selectedItems.push(item);
+          }
+          break;
+      }
     }
   });
-}
-
-
-if (this.FilterData.length > 0) {
-  this.syncCheckedStateForDropdown(this.costObjReqItemList, 'Requesting VPU Group');
-  this.syncCheckedStateForDropdown(this.costObjResItemList, 'Responsible VPU Group');
-  this.syncCheckedStateForDropdown(this.wpaReqItemList, 'WPA Requesting VPU Group');
-  this.syncCheckedStateForDropdown(this.wpaResItemList, 'WPA Responsible VPU Group');
 }
