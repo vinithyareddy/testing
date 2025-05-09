b66b7ce627945a85f90aa9f8feb0e7c04bcebace
vinithya <vinithyareddyp.com>
2025-05-08 20:19:02 -0400

    24

--- tet.ts
+++ tet.ts
@@ -1,28 +1,8 @@
-// Restore multiselect dropdown selections
-if (element2.title === 'Requesting VPU Group') {
-  const match = this.costObjReqItemList.find(x => x.itemName === filterData[1]);
-  if (match && !this.cost_obj_req_selectedItems.some(i => i.itemName === match.itemName)) {
-    this.cost_obj_req_selectedItems.push(match);
-  }
-}
-
-if (element2.title === 'Responsible VPU Group') {
-  const match = this.costObjResItemList.find(x => x.itemName === filterData[1]);
-  if (match && !this.cost_obj_res_selectedItems.some(i => i.itemName === match.itemName)) {
-    this.cost_obj_res_selectedItems.push(match);
-  }
-}
-
-if (element2.title === 'WPA Requesting VPU Group') {
-  const match = this.wpaReqItemList.find(x => x.itemName === filterData[1]);
-  if (match && !this.wpa_req_selectedItems.some(i => i.itemName === match.itemName)) {
-    this.wpa_req_selectedItems.push(match);
-  }
-}
-
-if (element2.title === 'WPA Responsible VPU Group') {
-  const match = this.wpaResItemList.find(x => x.itemName === filterData[1]);
-  if (match && !this.wpa_res_selectedItems.some(i => i.itemName === match.itemName)) {
-    this.wpa_res_selectedItems.push(match);
-  }
-}
+// ✅ Force UI to refresh selected items visually
+setTimeout(() => {
+  this.cost_obj_req_selectedItems = [...this.cost_obj_req_selectedItems];
+  this.cost_obj_res_selectedItems = [...this.cost_obj_res_selectedItems];
+  this.wpa_req_selectedItems = [...this.wpa_req_selectedItems];
+  this.wpa_res_selectedItems = [...this.wpa_res_selectedItems];
+  this.selectedItemsvpugroup = [...this.selectedItemsvpugroup];
+}, 0);
