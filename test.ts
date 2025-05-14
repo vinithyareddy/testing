4417a39a031e6af3f3127b27121944ed7e991f31
vinithya <vinithyareddyp.com>
2025-05-14 14:42:03 -0400

    4

--- test.ts
+++ test.ts
@@ -1,56 +1,6 @@
-missingHours: number = 0;
-adjustedHours: number = 0;
-missingPercentage: number = 0;
+this.ResponseFlagMissing = true;
 
-
-// After calculating closedMonth, closingMonth, openedMonth, and corresponding percentages
-this.missingHours = this.closedMonth + this.closingMonth + this.openedMonth;
-const totalPercent = this.closedHrPer + this.closingHrPer + this.openHrPer;
-
-if (totalPercent > 0) {
-  this.adjustedHours = Math.round(this.missingHours / (totalPercent / 100));
-  this.missingPercentage = Math.round((this.missingHours / this.adjustedHours) * 100);
-}
-
-
-import * as Highcharts from 'highcharts';
-
-ngAfterViewInit() {
+// Only draw the chart if adjustedHours is valid
+if (this.adjustedHours > 0 && this.missingHours >= 0) {
   this.renderCompliancePieChart();
 }
-renderCompliancePieChart() {
-    Highcharts.chart('compliancePieContainer', {
-      chart: {
-        type: 'pie',
-        height: 180
-      },
-      title: {
-        text: `${this.missingPercentage}%`,
-        verticalAlign: 'middle',
-        floating: true,
-        style: {
-          fontSize: '20px',
-          fontWeight: 'bold'
-        }
-      },
-      tooltip: {
-        pointFormat: '<b>{point.y} hours</b>'
-      },
-      plotOptions: {
-        pie: {
-          innerSize: '60%',
-          dataLabels: {
-            enabled: false
-          }
-        }
-      },
-      series: [{
-        name: 'Hours',
-        data: [
-          { name: 'Missing Time', y: this.missingHours, color: '#0056D2' },
-          { name: 'Adjusted Required', y: this.adjustedHours - this.missingHours, color: '#c4c4c4' }
-        ]
-      }]
-    });
-  }
-  
