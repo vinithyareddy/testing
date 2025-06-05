622b7977dd2167e091d9837ecf6d5f00b53fa1d0
vinithya <vinithyareddyp.com>
2025-06-05 14:19:39 -0400

    26

--- test.ts
+++ test.ts
@@ -1,30 +1,4 @@
-{
-  "/api": {
-    "target": "https://biservicesqa.worldbank.org",
-    "secure": false,
-    "changeOrigin": true,
-    "logLevel": "debug",
-    "pathRewrite": { "^/api": "" }
-  },
-  "/fc-api": {
-    "target": "https://mds.worldbank.org",
-    "secure": false,
-    "changeOrigin": true,
-    "logLevel": "debug",
-    "pathRewrite": { "^/fc-api": "" }
-  },
-  "/profile-api": {
-    "target": "https://reports.worldbank.org",
-    "secure": false,
-    "changeOrigin": true,
-    "logLevel": "debug",
-    "pathRewrite": { "^/profile-api": "" }
-  },
-  "/sharepoint-api": {
-    "target": "https://worldbankgroup.sharepoint.com",
-    "secure": false,
-    "changeOrigin": true,
-    "logLevel": "debug",
-    "pathRewrite": { "^/sharepoint-api": "" }
-  }
-}
+const isLocal = window.location.hostname.includes('localhost');
+const baseUrl = isLocal ? '/qa-api' : 'https://reports.worldbank.org';
+
+return this.Http.get(baseUrl + '/BIPortalSecurity/rest/bisecurity/getUserProfile?input={"userUpi":"' + upi + '"}');
