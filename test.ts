c9886b9cac56cdf68da9051d80da7e4e67730423
vinithya <vinithyareddyp.com>
2025-07-07 18:55:29 -0400

    44

--- test.ts
+++ test.ts
@@ -1,6 +1,11 @@
-test('Verify As Of Date Dropdown is Present', async ({ page }) => {
-  const dateDropdown = page.locator('text=As of Date');
-  await expect(dateDropdown).toBeAttached();
-  await expect(dateDropdown).toBeVisible();
-  await expect(dateDropdown).toHaveScreenshot('oh-ibrd-ida-as-of-date-dropdown.png');
+test('Verify Indicator Approval Status is 0 and 36', async ({ page }) => {
+  // Wait for relevant text
+  const zero = page.locator('text=0');
+  const thirtySix = page.locator('text=36');
+
+  await expect(zero).toBeVisible({ timeout: 10000 });
+  await expect(thirtySix).toBeVisible({ timeout: 10000 });
+
+  await expect(zero).toHaveScreenshot('oh-ibrd-ida-indicator-0.png');
+  await expect(thirtySix).toHaveScreenshot('oh-ibrd-ida-indicator-36.png');
 });
