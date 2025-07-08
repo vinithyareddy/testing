2b9b2f2ad86fd480c5db8241d59201bb414c2115
vinithya <vinithyareddyp.com>
2025-07-08 18:18:40 -0400

    66

--- test.ts
+++ test.ts
@@ -1,55 +1,44 @@
-test.describe('Guarantees Widget', () => {
+test.describe('Gross Outstanding Exposure Widget', () => {
 
-  const widgetSelector = '#print-section app-loan-equity-investment-outstanding';
-
-  test('Verify Guarantees Widget has Correct Title', async ({ page }) => {
-    await page.goto('https://mgmtqa.asestg.worldbank.org/operation_highlight/miga');
-
-    const widget = page.locator(widgetSelector);
+  test('Verify Gross Outstanding Exposure Widget has Title', async ({ page }) => {
+    const widget = page.locator('app-commitments-guarantees');
     await widget.scrollIntoViewIfNeeded();
-    await expect(widget).toBeVisible({ timeout: 10000 });
-    await expect(widget.getByText('Guarantees')).toBeVisible();
+    await expect(widget.getByText('Gross outstanding exposure')).toBeVisible();
   });
 
-  test('Verify Guarantees Widget has Tab sections', async ({ page }) => {
-    const widget = page.locator(widgetSelector);
-    await expect(widget).toMatchAriaSnapshot(`
-    - radio "FY TO DATE"
-    - text: FY TO DATE
-    - radio "QUARTER"
-    - text: QUARTER
-    - radio "3 YEARS AVG."
+  test('Verify Gross Outstanding Exposure Widget has Tab sections', async ({ page }) => {
+    await expect(page.locator('app-commitments-guarantees')).toMatchAriaSnapshot(`
+      - radio "FY TO DATE"
+      - text: FY TO DATE
+      - radio "QUARTER"
+      - text: QUARTER
+      - radio "3 YEARS AVG."
     `);
   });
 
-  test('Verify Guarantees Widget Title Tooltip', async ({ page }) => {
-    const widget = page.locator(widgetSelector);
-    await page.waitForTimeout(2000);
-
-    await widget.getByRole('link').click();
+  test('Verify Gross Outstanding Exposure Widget Title Tooltip', async ({ page }) => {
+    await page.waitForTimeout(1000);
+    await page.locator('app-commitments-guarantees').getByRole('link').click();
     await expect(page.locator('h3.popover-title')).toMatchAriaSnapshot(`
-    - heading "Guarantees Close" [level=3]:
-          - button "Close"
+      - heading "Gross Outstanding Close" [level=3]:
+        - button "Close"
     `);
     await page.getByRole('button', { name: 'Close' }).click();
   });
 
-  test('Verify Guarantees Widget FY TO DATE section selected', async ({ page }) => {
-    const widget = page.locator(widgetSelector);
-    await widget.getByText('FY TO DATE').click();
-    await expect(widget).toHaveScreenshot('oh-miga-card-app-loan-equity-investment-outstanding-FY-screenshot.png');
+  test('Verify Gross Outstanding Exposure FY TO DATE section selected', async ({ page }) => {
+    await page.locator('app-commitments-guarantees').getByText('FY TO DATE').click();
+    await expect(page.locator('app-commitments-guarantees')).toHaveScreenshot('oh-miga-card-app-commitments-guarantees-FY-screenshot.png');
   });
 
-  test('Verify Guarantees Widget QUARTER section selected', async ({ page }) => {
-    const widget = page.locator(widgetSelector);
-    await widget.getByText('QUARTER').click();
-    await expect(widget).toHaveScreenshot('oh-miga-card-app-loan-equity-investment-outstanding-quarter-screenshot.png');
+  test('Verify Gross Outstanding Exposure QUARTER section selected', async ({ page }) => {
+    await page.locator('app-commitments-guarantees').getByText('QUARTER').click();
+    await expect(page.locator('app-commitments-guarantees')).toHaveScreenshot('oh-miga-card-app-commitments-guarantees-quarter-screenshot.png');
   });
 
-  test('Verify Guarantees Widget 3 Years Avg section selected', async ({ page }) => {
-    const widget = page.locator(widgetSelector);
-    await widget.getByText('YEARS AVG.').click();
-    await expect(widget).toHaveScreenshot('oh-miga-card-app-loan-equity-investment-outstanding-3years-avg-screenshot.png');
+  test('Verify Gross Outstanding Exposure 3 Years Avg section selected', async ({ page }) => {
+    await page.locator('app-commitments-guarantees').getByText('3 YEARS AVG.').click();
+    await expect(page.locator('app-commitments-guarantees')).toHaveScreenshot('oh-miga-card-app-commitments-guarantees-3years-avg-screenshot.png');
   });
 
 });
