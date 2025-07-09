249ba0edfb1d5b4096fc0cb19e851281130d2f53
vinithya <vinithyareddyp.com>
2025-07-09 13:59:24 -0400

    79

--- test.ts
+++ test.ts
@@ -1,10 +1,23 @@
-test('Verify Guarantees Widget FY TO DATE section selected', async ({ page }) => {
-  const widget = page.locator(widgetSelector);
-  await widget.getByText('FY TO DATE').click();
-  await page.waitForTimeout(1000); // let it settle
-  await widget.scrollIntoViewIfNeeded();
-
-  await expect(widget).toHaveScreenshot('oh-miga-card-app-equity-investment-outstanding-FY-screenshot.png', {
-    maxDiffPixelRatio: 0.03,
+test('Verify IBRD widget titles are visible', async ({ page }) => {
+  await page.goto('https://mgmtqa.asestg.worldbank.org/operation_highlight/ibrd', {
+    waitUntil: 'domcontentloaded',
   });
+
+  const widgetTitles = [
+    'Commitments',
+    'Gross Disbursements',
+    'Net Flows (IBRD)',
+    'Loans Outstanding',
+    'Undisbursed Balances',
+  ];
+
+  for (const title of widgetTitles) {
+    const titleLocator = page.getByText(title, { exact: true });
+
+    // Scroll to the element if it's not in view
+    await titleLocator.scrollIntoViewIfNeeded();
+
+    // Assert visibility
+    await expect(titleLocator).toBeVisible({ timeout: 15000 });
+  }
 });
