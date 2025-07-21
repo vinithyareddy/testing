b8c2327a0717028522351f1467aa62aec4e83a4c
vinithya <vinithyareddyp.com>
2025-07-21 13:46:15 -0400

    247

--- test.ts
+++ test.ts
@@ -5,11 +5,13 @@ test('Verify Final Plans vs Actuals Title is Visible', async ({ page }) => {
 });
 
 test('Verify Menu Tab Click Works', async ({ page }) => {
-  const menuTab = page.locator('#mat-button-toggle-84-button'); // corrected selector
+  const menuTab = page.locator('#mat-button-toggle-84-button');
   await expect(menuTab).toBeVisible({ timeout: 10000 });
+
   await menuTab.click();
   await expect(menuTab).toHaveScreenshot('sr-fp-vs-actual-fundgroup-menu-tab.png');
 });
+
 
 test('Verify Table Tab Click Works', async ({ page }) => {
   const tableTab = page.locator('#mat-button-toggle-85-button');
