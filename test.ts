bddd067062349eb8d26c562f41d3b69a969f9371
vinithya <vinithyareddyp.com>
2025-07-22 14:06:22 -0400

    260

--- test.ts
+++ test.ts
@@ -1,9 +1,26 @@
-test('Verify Expand Icon in Widget', async ({ page }) => {
+test('Verify widget Toggle button', async ({ page }) => {
   const widget = page.locator('app-time-in-error');
-  await expect(widget).toBeVisible({ timeout: 10000 });
+  await widget.waitFor({ state: 'visible', timeout: 10000 });
 
-  const expand = widget.locator('span[class*="bgt-collapse-state"]'); // More specific
-  await expect(expand).toBeVisible({ timeout: 10000 });
+  const toggleAll = page.locator('[aria-label="All"] .mat-radio-outer-circle');
+  const toggleBB = page.locator('[aria-label="BB"] .mat-radio-outer-circle');
+  const toggleReimb = page.locator('[aria-label="REIMB"] .mat-radio-outer-circle');
+  const toggleTF = page.locator('[aria-label="TF"] .mat-radio-outer-circle');
 
-  await expect(expand).toHaveScreenshot('sr-trs-overview-time-in-error-expand-icon.png');
+  await toggleAll.waitFor({ state: 'visible', timeout: 10000 });
+  await toggleAll.click({ force: true });
+  await page.waitForTimeout(1000);
+  await expect(widget).toHaveScreenshot('sr-trs-overview-time-in-error-toggle-all.png');
+
+  await toggleBB.click({ force: true });
+  await page.waitForTimeout(1000);
+  await expect(widget).toHaveScreenshot('sr-trs-overview-time-in-error-toggle-bb.png');
+
+  await toggleReimb.click({ force: true });
+  await page.waitForTimeout(1000);
+  await expect(widget).toHaveScreenshot('sr-trs-overview-time-in-error-toggle-reimb.png');
+
+  await toggleTF.click({ force: true });
+  await page.waitForTimeout(1000);
+  await expect(widget).toHaveScreenshot('sr-trs-overview-time-in-error-toggle-tf.png');
 });
