5c720b50c0fdb4146888c07b2f5235c64bb919e4
vinithya <vinithyareddyp.com>
2025-07-25 16:12:42 -0400

    323

--- test.ts
+++ test.ts
@@ -1,9 +1,1 @@
-test('Verify Money Bag Icon is Visible', async ({ page }) => {
-  await page.waitForTimeout(3000);
-
-  const burnRateWidget = page.locator('div.budget-box-h1').first();
-  const icon = burnRateWidget.locator('img[src*="icon.svg"]');
-
-  await expect(icon).toBeVisible({ timeout: 10000 });
-  await expect(icon).toHaveScreenshot('sr-wpa-burn-rate-moneybag-icon.png');
-});
+const toggleAll = page.locator('input[type="radio"][value="all"]');
