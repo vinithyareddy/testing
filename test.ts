4fc53d7669dfba07fef9697c337f9e5a2fceadae
vinithya <vinithyareddyp.com>
2025-07-26 15:56:16 -0400

    330

--- test.ts
+++ test.ts
@@ -1,6 +1,8 @@
 test('Verify Filters Summary Bar is visible and take screenshot', async ({ page }) => {
-  await page.waitForTimeout(1000); // or wait for some key element to load
+  await page.waitForTimeout(2000); // give time for data to load
 
-  const filterSummaryBar = page.locator('div.banner-align-p-0');
-  await expect(filterSummaryBar).toBeVisible({ timeout: 10000 });
+  const filterSummaryBar = page.locator('[class*="banner-align-p-0"]');
+  await expect(filterSummaryBar).toBeVisible({ timeout: 15000 });
+
+  await filterSummaryBar.screenshot({ path: 'sr-budget-expenses-filter-summary-bar-open.png' });
 });
