7b5e6728b4f824befb0c0533b2f8452f78d2166e
vinithya <vinithyareddyp.com>
2025-07-27 15:19:59 -0400

    332

--- test.ts
+++ test.ts
@@ -1,6 +1,12 @@
-const fullscreenIcon = page.locator('span.view >> img');
-await expect(fullscreenIcon).toBeVisible({ timeout: 10000 });
+test('Verify Fullscreen Icon is Clickable in Sources Widget', async ({ page }) => {
+  const widget = page.locator('#Dashboard app-source-users');
+  await expect(widget).toBeVisible({ timeout: 10000 });
 
-await fullscreenIcon.click();
-await page.waitForTimeout(3000);
-await expect(page).toHaveScreenshot('sr-sources-uses-widget-fullscreen.png');
+  const fullscreenIcon = page.locator('span.view').nth(1);
+  await expect(fullscreenIcon).toBeVisible({ timeout: 10000 });
+
+  await fullscreenIcon.click();
+  await page.waitForTimeout(3000);
+
+  await expect(page).toHaveScreenshot('sr-sources-uses-widget-fullscreen.png');
+});
