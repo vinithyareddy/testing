fd62fa43c807df6e30bbc8b726e383e7b9fb7373
vinithya <vinithyareddyp.com>
2025-08-02 17:50:48 -0400

    361

--- test.ts
+++ test.ts
@@ -1,3 +1,10 @@
-const tableIcon = page.locator('mat-icon.mat-icon[aria-label="table_view"]');
-await tableIcon.waitFor({ state: 'visible', timeout: 10000 });
+await widget.scrollIntoViewIfNeeded();
+
+const tableIcon = widget.locator('button.mat-button-toggle span.mat-button-toggle-label-content');
+
+await tableIcon.waitFor({ state: 'visible', timeout: 12000 });
+await expect(tableIcon).toHaveScreenshot('qa-dashboard-other-management-attention-widget-table-icon-visible.png');
+
 await tableIcon.click();
+await page.waitForTimeout(3000);
+await expect(widget).toHaveScreenshot('qa-dashboard-other-management-attention-widget-table-tab-data.png');
