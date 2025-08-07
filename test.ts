d6e97a4c66f69cdf12d84fb31f30a654d53fc729
vinithya <vinithyareddyp.com>
2025-08-07 16:06:11 -0400

    373

--- test.ts
+++ test.ts
@@ -1,12 +1,25 @@
 <div class="row tile-widget-bottomtext">
   <div class="col-sm-12 d-flex flex-column">
-    <div class="d-flex align-items-center">
-      <div class="budget-box-h3 textEliipsis">Forecast Sources</div>
-      <div class="budget-box-right ml-2">{{TotalSouces | number}}{{filterUnit}}</div>
+    <div class="d-flex align-items-center mt-3">
+      <div class="budget-box-h3 textEliipsis mb-0">Forecast Sources</div>
+      <div class="budget-box-right mb-0 ml-2">{{TotalSouces | number}}{{filterUnit}}</div>
     </div>
     <div class="d-flex align-items-center mt-1">
-      <div class="budget-box-h3 textEliipsis">Forecast Uses</div>
-      <div class="budget-box-right ml-2">{{TotalUses | number}}{{filterUnit}}</div>
+      <div class="budget-box-h3 textEliipsis mb-0">Forecast Uses</div>
+      <div class="budget-box-right mb-0 ml-2">{{TotalUses | number}}{{filterUnit}}</div>
     </div>
   </div>
 </div>
+
+
+/* keep pairs tight and aligned */
+.box-md .tile-widget-bottomtext .d-flex {
+  justify-content: flex-start;
+}
+
+.box-md .tile-widget-bottomtext .budget-box-right {
+  /* small gap between label and value */
+  margin-left: .5rem;
+  /* keep numbers aligned on baseline with label text */
+  line-height: 1;
+}
