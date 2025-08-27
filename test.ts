33a03ebedad6d1089c5a201031cab787d38e302d
vinithya <vinithyareddyp.com>
2025-08-27 13:09:14 -0400

    399

--- test.ts
+++ test.ts
@@ -1,13 +1,13 @@
 <input type="checkbox" class="form-check-input"
-id = "{{resData[childData.measureQuery['code']]}}~{{i}}{{j}}{{k}}"
-title = "{{resData[childData.measureQuery['name']]}}"
-alt = "{{childData.title}}"[attr.disabled] = "resData.disable"
-value = "{{resData[childData.measureQuery['code']]}}"
+[id] = "resData[childData.measureQuery['code']] + '~' + i + j + k"
+[title] = "resData[childData.measureQuery['name']]"
+[attr.alt] = "childData.title"
+[attr.disabled] = "resData.disable"
+[value] = "resData[childData.measureQuery['code']]"
 [(ngModel)] = "resData.checked"
-  (click) = "onhandleclick($event,childData,refineData)"
-  [disabled] = "(childData.key === 'bgi%1' && resData[childData.measureQuery['code']] === 'N') ? true : false" >
+  (change) = "onhandleclick(resData, childData)"
+  [disabled] = "(childData.key === 'bgi%1' && resData[childData.measureQuery['code']] === 'N')" >
 
   <label class="form-check-label small-font"
-for= "{{resData[childData.measureQuery['code']]}}~{{i}}{{j}}{{k}}"
-[innerHTML] = "resData[childData.measureQuery['name']]"
-  (click) = "onhandleclick($event,childData,refineData)" > </label>
+  [for]="resData[childData.measureQuery['code']] + '~' + i + j + k"
+  [innerHTML] = "resData[childData.measureQuery['name']]" > </label>
