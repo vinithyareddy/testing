6f247dd5ee3ba373881a8f8ef4c64e9fbcaefe3d
vinithya <vinithyareddyp.com>
2025-08-27 15:40:15 -0400

    408

--- test.ts
+++ test.ts
@@ -1,10 +1,4 @@
-<input
-  type="radio"
-class="form-check-input"
-[id] = "resData[childData.measureQuery['code']] + '_' + i + '_' + j"
-name = "group_{{i}}_{{j}}"
-[title] = "resData[childData.measureQuery['name']]"
-[value] = "resData[childData.measureQuery['code']]"
-[checked] = "resData.checked"
-  (change) = "onhandleRadio(childData, resData)"
-  />
+<label class="form-check-label small-font marleft"
+[innerHTML] = "resData[childData.measureQuery['name']]"
+  (click) = "onhandleDropdownclick($event, childData, refineData, resData[childData.measureQuery['code']], resData)" >
+  </label>
