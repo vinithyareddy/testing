e580d9e5f23e55eec875378bf922194da6419c2e
vinithya <vinithyareddyp.com>
2025-08-27 14:20:05 -0400

    404

--- test.ts
+++ test.ts
@@ -1,20 +1,19 @@
-onhandleclick(childData: any, resData: any) {
-  const category = childData.title;
-  const value = resData[childData.measureQuery['code']];
-  const name = resData[childData.measureQuery['name']];
-  const id = childData.key;
+onhandlesubChildclick(curntdata: any, childdata: any, vpu: any) {
+  const category = curntdata.title;
+  const value = curntdata[curntdata.childfacet];
+  const name = curntdata[curntdata.childfacet];
+  const id = curntdata.id;
   const Typenumber = (category === 'Approval FY') ? 'number' : 'string';
 
-  // if already selected, remove
   const idx = this.FilterData.findIndex(x => x.value === value && x.category === category);
   if (idx > -1) {
     this.FilterData.splice(idx, 1);
-    resData.checked = false;
+    curntdata.checked = false;
   } else {
-    this.FilterData.push({ category, value, name, id, facetType: childData.facetQuery, Typenumber, vpucategory: '' });
-    resData.checked = true;
+    this.FilterData.push({ category, value, name, id, facetType: curntdata.facetQuery, Typenumber, vpucategory: vpu });
+    curntdata.checked = true;
   }
 
   this.FilterCount = this.FilterData.length;
-  this.clearFilterFlag = '1';
+  this.clearFilterFlag = '2';
 }
