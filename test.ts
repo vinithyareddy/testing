7ce3f65d3e1abd00aac1ac372de0d8a783b5b770
vinithya <vinithyareddyp.com>
2025-08-27 13:11:14 -0400

    400

--- test.ts
+++ test.ts
@@ -1,13 +1,11 @@
 <input type="checkbox" class="form-check-input"
-[id] = "resData[childData.measureQuery['code']] + '~' + i + j + k"
-[title] = "resData[childData.measureQuery['name']]"
-[attr.alt] = "childData.title"
-[attr.disabled] = "resData.disable"
-[value] = "resData[childData.measureQuery['code']]"
-[(ngModel)] = "resData.checked"
-  (change) = "onhandleclick(resData, childData)"
-  [disabled] = "(childData.key === 'bgi%1' && resData[childData.measureQuery['code']] === 'N')" >
+[id] = "data1.crntdata[data1.crntdata.childfacet]"
+[value] = "data1.crntdata[data1.crntdata.childfacet]"
+[attr.disabled] = "data1.crntdata.disable"
+[(ngModel)] = "data1.crntdata.checked"
+  (change) = "onhandlesubChildclick(data1.crntdata, data1.subdata, data1.vpuname)" >
 
   <label class="form-check-label small-font"
-  [for]="resData[childData.measureQuery['code']] + '~' + i + j + k"
-  [innerHTML] = "resData[childData.measureQuery['name']]" > </label>
+  [for]="data1.crntdata[data1.crntdata.childfacet]" >
+    {{ data1.crntdata[data1.crntdata.childfacet] }}
+</label>
