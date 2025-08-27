c2d3c46b8bfda118da312c2206213efd0ead1993
vinithya <vinithyareddyp.com>
2025-08-27 13:08:09 -0400

    398

--- test.ts
+++ test.ts
@@ -1,14 +1,13 @@
-"serve": {
-  "builder": "@angular-devkit/build-angular:dev-server",
-    "options": {
-    "browserTarget": "coreframework:build"
-  },
-  "configurations": {
-    "production": {
-      "browserTarget": "coreframework:build:production"
-    },
-    "development": {
-      "browserTarget": "coreframework:build:development"
-    }
-  }
-}
+<input type="checkbox" class="form-check-input"
+id = "{{resData[childData.measureQuery['code']]}}~{{i}}{{j}}{{k}}"
+title = "{{resData[childData.measureQuery['name']]}}"
+alt = "{{childData.title}}"[attr.disabled] = "resData.disable"
+value = "{{resData[childData.measureQuery['code']]}}"
+[(ngModel)] = "resData.checked"
+  (click) = "onhandleclick($event,childData,refineData)"
+  [disabled] = "(childData.key === 'bgi%1' && resData[childData.measureQuery['code']] === 'N') ? true : false" >
+
+  <label class="form-check-label small-font"
+for= "{{resData[childData.measureQuery['code']]}}~{{i}}{{j}}{{k}}"
+[innerHTML] = "resData[childData.measureQuery['name']]"
+  (click) = "onhandleclick($event,childData,refineData)" > </label>
