8619bcd2f5be6f93003791efc1355bde1cbf0ab3
vinithya <vinithyareddyp.com>
2025-08-27 14:45:32 -0400

    407

--- test.ts
+++ test.ts
@@ -1,18 +1,10 @@
-onhandleRadio(childData: any, resData: any) {
-  const category = childData.title;
-  const value = resData[childData.measureQuery['code']];
-  const name = resData[childData.measureQuery['name']];
-  const id = childData.key;
-  const Typenumber = (category === 'Approval FY') ? 'number' : 'string';
-
-  // clear old selection
-  this.FilterData = this.FilterData.filter(x => x.category !== category);
-
-  // mark checked
-  (childData.data || []).forEach((r: any) => r.checked = false);
-  resData.checked = true;
-
-  this.FilterData.push({ category, value, name, id, facetType: childData.facetQuery, Typenumber, vpucategory: '' });
-  this.FilterCount = this.FilterData.length;
-  this.clearFilterFlag = '1';
-}
+<input
+  type="radio"
+class="form-check-input"
+[id] = "resData[childData.measureQuery['code']] + '_' + i + '_' + j"
+name = "group_{{i}}_{{j}}"
+[title] = "resData[childData.measureQuery['name']]"
+[value] = "resData[childData.measureQuery['code']]"
+[checked] = "resData.checked"
+  (change) = "onhandleRadio(childData, resData)"
+  />
