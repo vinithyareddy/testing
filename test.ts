4503f18de89bd9e2a0f4e5897fdb7e600f3c7907
vinithya <vinithyareddyp.com>
2025-08-27 14:17:45 -0400

    403

--- test.ts
+++ test.ts
@@ -1,27 +1,20 @@
-<input type="checkbox" class="form-check-input"
-[id] = "resData[childData.measureQuery['code']] + '~' + i + j + k"
-[title] = "resData[childData.measureQuery['name']]"
-[attr.alt] = "childData.title"
-[attr.disabled] = "resData.disable"
-[value] = "resData[childData.measureQuery['code']]"
-[(ngModel)] = "resData.checked"
-  (change) = "onhandleclick(resData, childData)"
-  [disabled] = "(childData.key === 'bgi%1' && resData[childData.measureQuery['code']] === 'N')" >
+onhandleclick(childData: any, resData: any) {
+  const category = childData.title;
+  const value = resData[childData.measureQuery['code']];
+  const name = resData[childData.measureQuery['name']];
+  const id = childData.key;
+  const Typenumber = (category === 'Approval FY') ? 'number' : 'string';
 
-  <label class="form-check-label small-font"
-  [for]="resData[childData.measureQuery['code']] + '~' + i + j + k"
-  [innerHTML] = "resData[childData.measureQuery['name']]" > </label>
+  // if already selected, remove
+  const idx = this.FilterData.findIndex(x => x.value === value && x.category === category);
+  if (idx > -1) {
+    this.FilterData.splice(idx, 1);
+    resData.checked = false;
+  } else {
+    this.FilterData.push({ category, value, name, id, facetType: childData.facetQuery, Typenumber, vpucategory: '' });
+    resData.checked = true;
+  }
 
-
-
-    < input type = "checkbox" class="form-check-input"
-    [id] = "data1.crntdata[data1.crntdata.childfacet]"
-    [value] = "data1.crntdata[data1.crntdata.childfacet]"
-    [attr.disabled] = "data1.crntdata.disable"
-    [(ngModel)] = "data1.crntdata.checked"
-      (change) = "onhandlesubChildclick(data1.crntdata, data1.subdata, data1.vpuname)" >
-
-      <label class="form-check-label small-font"
-      [for]="data1.crntdata[data1.crntdata.childfacet]" >
-        {{ data1.crntdata[data1.crntdata.childfacet] }}
-</label>
+  this.FilterCount = this.FilterData.length;
+  this.clearFilterFlag = '1';
+}
