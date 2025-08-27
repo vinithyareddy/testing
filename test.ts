ca0ad8c5018b459b838fcc9164c78a5386ef54e0
vinithya <vinithyareddyp.com>
2025-08-27 13:12:46 -0400

    401

--- test.ts
+++ test.ts
@@ -1,11 +1,51 @@
-<input type="checkbox" class="form-check-input"
-[id] = "data1.crntdata[data1.crntdata.childfacet]"
-[value] = "data1.crntdata[data1.crntdata.childfacet]"
-[attr.disabled] = "data1.crntdata.disable"
-[(ngModel)] = "data1.crntdata.checked"
-  (change) = "onhandlesubChildclick(data1.crntdata, data1.subdata, data1.vpuname)" >
+onhandleclick(resData: any, childData: any) {
+  this.FilterCount = 0;
+  const category = childData.title;
+  const value = resData[childData.measureQuery['code']];
+  const name = resData[childData.measureQuery['name']];
+  const id = childData.key;
+  const Typenumber = (category === 'Approval FY') ? 'number' : 'string';
 
-  <label class="form-check-label small-font"
-  [for]="data1.crntdata[data1.crntdata.childfacet]" >
-    {{ data1.crntdata[data1.crntdata.childfacet] }}
-</label>
+  const idx = this.FilterData.findIndex(x => x.value === value && x.category === category);
+
+  if (resData.checked && idx === -1) {
+    this.FilterData.push({
+      category, value, name, id,
+      facetType: childData.facetQuery,
+      Typenumber,
+      vpucategory: ''
+    });
+  } else if (!resData.checked && idx !== -1) {
+    this.FilterData.splice(idx, 1);
+  }
+  this.FilterCount = this.FilterData.length;
+  this.clearFilterFlag = '1';
+}
+onhandlesubChildclick(crntdata: any, childdata: any, vpu: string) {
+  this.FilterCount = 0;
+  const category = crntdata.title;
+  const value = crntdata[crntdata.childfacet];
+  const name = crntdata[crntdata.childfacet];
+  const id = crntdata.id;
+  const Typenumber = (category === 'Approval FY') ? 'number' : 'string';
+
+  const idx = this.FilterData.findIndex(x => x.value === value && x.category === category);
+
+  if (crntdata.checked && idx === -1) {
+    const filterdata = {
+      category, value, name, id,
+      checkedbox: true,
+      facetType: crntdata.facetQuery,
+      Typenumber,
+      vpucategory: vpu
+    };
+    this.tempFilterData.push(filterdata);
+    this.FilterData.push(filterdata);
+  } else if (!crntdata.checked && idx !== -1) {
+    this.FilterData.splice(idx, 1);
+  }
+
+  this.FilterCount = this.FilterData.length;
+  this.clearFilterFlag = '2';
+  this.refineload = 2;
+}
