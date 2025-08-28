a47b85f6d65d8b2c5c73d041ccb3cd087a6b1244
vinithya <vinithyareddyp.com>
2025-08-28 18:28:43 -0400

    517

--- test.ts
+++ test.ts
@@ -1,129 +1,48 @@
-<div class="budget-card-box" #chartsection>
-  <div class="budget-box-chart">
+import { Component, Renderer2 } from '@angular/core';
 
-    <!-- Header -->
-    <div class="card-box-header-sec d-flex justify-content-between align-items-center">
-      <!-- Title -->
-      <div class="widget-heading pointer mt-1">
-        <span>
-          Workforce Supply (FTE) by FCV Status
-          <ng-template [ngTemplateOutlet]="infotemp"></ng-template>
-        </span>
-      </div>
+@Component({
+  selector: 'app-swfp-by-country-job',
+  templateUrl: './swfp-by-country-job.component.html',
+  styleUrls: ['./swfp-by-country-job.component.scss']
+})
+export class SwfpByCountryJobComponent {
+  ResponseFlag = true;   // simulate data load complete
+  collapsed = false;
+  fullview = false;
 
-      <!-- Icons -->
-      <div class="header-icons">
-        <div class="lft-toggle" [class.lft-toggle-active]="widgetType == 'th'" (click)="loadWidget('th')">
-          <i class="fas fa-chart-bar" aria-hidden="true"></i>
-        </div>
-        <div class="rgt-toggle" [class.rgt-toggle-active]="widgetType == 'ch'" (click)="loadWidget('ch')">
-          <i class="far fa-chart-pie" aria-hidden="true"></i>
-        </div>
-        <div class="ellipsis">
-          <i class="fas fa-ellipsis-v"></i>
-        </div>
-      </div>
-    </div>
+  // Demo data (replace with API/service later)
+  groupdata = [
+    { country: 'United States', flag: 'us', fte: 96, expanded: false },
+    { country: 'India', flag: 'in', fte: 10, expanded: false },
+    { country: 'Nigeria', flag: 'ng', fte: 6, expanded: false },
+    { country: 'Bangladesh', flag: 'bd', fte: 5, expanded: false },
+    { country: 'Ethiopia', flag: 'et', fte: 5, expanded: false },
+    { country: 'Kenya', flag: 'ke', fte: 5, expanded: false },
+    { country: 'Pakistan', flag: 'pk', fte: 5, expanded: false },
+    { country: 'Senegal', flag: 'sn', fte: 4, expanded: false }
+  ];
 
-    <!-- Body -->
-    <div [@collapse]="collapsed">
-      <!-- Loader -->
-      <ng-container *ngIf="!ResponseFlag">
-        <div class="loader-img">
-          <lift-section-loader></lift-section-loader>
-        </div>
-      </ng-container>
+  total = [{ label: 'Total', fte: 239 }];
 
-      <!-- Chart View -->
-      <ng-container *ngIf="ResponseFlag">
-        <ng-container *ngIf="widgetType == 'ch'">
-          <div class="inner-card-box">
-            <highcharts-chart
-              [Highcharts]="Highcharts"
-              [options]="chartOptions"
-              [constructorType]="'chart'"
-              style="width: 100%; height: 250px; display: block;">
-            </highcharts-chart>
-          </div>
-        </ng-container>
+  constructor(private render: Renderer2) {}
 
-        <!-- View More -->
-        <div class="viewmore pointer mt-3 pt-3" (click)="getDetailPage()">
-          <span>View More&nbsp;&nbsp;</span><i class="fa fa-angle-right"></i>
-        </div>
-      </ng-container>
-    </div>
-  </div>
-</div>
+  expandRow(row: any) {
+    row.expanded = !row.expanded;
+  }
 
-<ng-template #infotemp>
-  <lift-popover popoverTitle="Workforce Supply (FTE) by FCV Status" popoverText="" [config]="config1">
-    <span><i class="far fa-info-circle"></i></span>
-  </lift-popover>
-</ng-template>
+  expand() { this.collapsed = false; }
+  collapse() { this.collapsed = true; }
 
-
-.budget-card-box {
-  background: #fff;
-  box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.1);
-  margin-top: 25px;
-
-  .widget-heading {
-    margin-right: 20px;
-
-    i.fa-info-circle {
-      font-size: 14px;   // smaller info icon
-      color: #0071bc;    // brand blue
-      margin-left: 6px;  // spacing from title
-      cursor: pointer;
+  fullPageView() {
+    this.fullview = !this.fullview;
+    if (this.fullview) {
+      this.render.addClass(document.body, 'no-scroll');
+    } else {
+      this.render.removeClass(document.body, 'no-scroll');
     }
   }
 
-  .header-icons {
-    margin-bottom: 30px;
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-    gap: 8px;
-
-    div {
-      width: 28px;
-      height: 28px;
-      border: 1px solid #d6d6d6;
-      text-align: center;
-      line-height: 28px;
-      cursor: pointer;
-
-      i {
-        font-size: 14px;
-      }
-    }
-
-    .ellipsis {
-      border: none; 
-      color: #0071bc; // ellipsis icon blue
-    }
+  getDetailPage() {
+    console.log('Navigate → detail page');
   }
-
-  .inner-card-box {
-    padding: 40px 0 10px 0;
-  }
-
-  .viewmore {
-    font-size: 13px;
-    font-weight: 500;
-    color: #0071bc;
-    text-align: right;
-  }
-}
-
-// icon rotations
-.fa-chart-bar {
-  display: inline-block;
-  transform: rotate(270deg) scaleY(-1);
-}
-
-.fa-chart-pie {
-  display: inline-block;
-  transform: rotate(180deg) scaleY(-1);
 }
