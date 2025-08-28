5e0c1bde5b15eec1d085b2f1f4efe2c639373826
vinithya <vinithyareddyp.com>
2025-08-28 16:31:56 -0400

    496

--- test.ts
+++ test.ts
@@ -1,7 +1,18 @@
-chart: {
-  type: 'pie',
-  spacingTop: 0,
-  spacingBottom: 0,
-  spacingLeft: 0,
-  spacingRight: 0,  // 🔑 removes uneven padding
-},
+<div class="row">
+  <!-- First card takes 5/12 -->
+  <div class="col-md-5">
+    <app-swfp-by-country-job></app-swfp-by-country-job>
+  </div>
+
+  <!-- Remaining 7/12 split equally -->
+  <div class="col-md-7">
+    <div class="row">
+      <div class="col-md-6">
+        <app-swfp-by-fcv-status></app-swfp-by-fcv-status>
+      </div>
+      <div class="col-md-6">
+        <app-swfp-by-location></app-swfp-by-location>
+      </div>
+    </div>
+  </div>
+</div>
