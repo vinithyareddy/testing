d92ddd4ef15cde109ef06183e8225c79f6c64b4e
vinithya <vinithyareddyp.com>
2025-08-28 16:50:13 -0400

    502

--- test.ts
+++ test.ts
@@ -1,24 +1,13 @@
-<div class="row card-box-header-sec align-items-center">
-  <!-- Title -->
-  <div class="widget-heading pointer mt-1 col-md-9">
-    <span>
-      Workforce Supply (FTE) by location
-      <ng-template [ngTemplateOutlet]="infotemp"></ng-template>
-    </span>
-  </div>
-
-  <!-- Icons -->
-  <div class="col-md-3 d-flex justify-content-end align-items-center">
-    <div class="togglebtn">
-      <div class="lft-toggle" [class.lft-toggle-active]="widgetType == 'th'" (click)="loadWidget('th')">
-        <i class="fas fa-chart-bar" aria-hidden="true"></i>
-      </div>
-      <div class="rgt-toggle" [class.rgt-toggle-active]="widgetType == 'ch'" (click)="loadWidget('ch')">
-        <i class="far fa-chart-pie" aria-hidden="true"></i>
-      </div>
-      <div class="ellipsis">
-        <i class="fas fa-ellipsis-v"></i>
-      </div>
+<div class="col-md-3 d-flex justify-content-end align-items-center">
+  <div class="togglebtn">
+    <div class="lft-toggle" [class.lft-toggle-active]="widgetType == 'th'" (click)="loadWidget('th')">
+      <i class="fas fa-chart-bar" aria-hidden="true"></i>
+    </div>
+    <div class="rgt-toggle" [class.rgt-toggle-active]="widgetType == 'ch'" (click)="loadWidget('ch')">
+      <i class="far fa-chart-pie" aria-hidden="true"></i>
+    </div>
+    <div class="ellipsis">
+      <i class="fas fa-ellipsis-v"></i>
     </div>
   </div>
 </div>
@@ -27,8 +16,8 @@
 .togglebtn {
   display: flex;
   align-items: center;
-  justify-content: flex-end;  // pushes all icons to right
-  gap: 10px;                  // space between icons
+  justify-content: flex-end;  // push icons all the way right
+  gap: 10px;
 
   div {
     width: 28px;
@@ -44,6 +33,6 @@
   }
 
   .ellipsis {
-    border: none;  // optional: remove box from ellipsis
+    border: none; // ellipsis looks cleaner
   }
 }
