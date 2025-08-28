a257edf10531c3e7ce7195d7f13a219e2b8320df
vinithya <vinithyareddyp.com>
2025-08-28 14:55:54 -0400

    483

--- test.ts
+++ test.ts
@@ -1,15 +1,19 @@
-tbody {
-  td:first-child {
-    text-align: left;
+<td class="pointer text-left" (click)="expandRow(row)">
+  <span class="cell-content">
+    <i *ngIf="!row.expanded" class="far fa-plus-circle"></i>
+    <i *ngIf="row.expanded" class="far fa-minus-circle"></i>
+    <img [src]="'https://flagcdn.com/16x12/' + row.flag + '.png'" class="flag-icon" />
+    {{ row.country }}
+  </span>
+</td>
 
-    .cell-content {
-      display: inline-flex;
-      align-items: center;
-      gap: 8px;
-    }
 
-    .cell-content i {
-      margin-right: 4px; // 🔑 new line
-    }
-  }
+.cell-content {
+  display: inline-flex;
+  align-items: center;
+  gap: 8px;     // space between icon, flag, and country text
+}
+
+.cell-content i {
+  margin-right: 6px; // specifically adds space after the + / - icon
 }
