de66cc6b58af4642ccfed0161bb609a580264803
vinithya <vinithyareddyp.com>
2025-08-28 14:10:52 -0400

    478

--- test.ts
+++ test.ts
@@ -5,31 +5,49 @@
   </tr>
 </thead>
 <tbody>
-  <tr *ngFor="let data of groupdata">
-    <td class="text-left">
-      <i class="far fa-plus-circle"></i> &nbsp; {{data.country}}
+  <tr *ngFor="let row of groupdata">
+    <td class="pointer text-left" (click)="expandRow(row)">
+      <i *ngIf="!row.expanded" class="far fa-plus-circle"></i>
+      <i *ngIf="row.expanded" class="far fa-minus-circle"></i>
+      <img [src]="'https://flagcdn.com/16x12/' + row.flag + '.png'" class="flag-icon" />
+      &nbsp;{{ row.country }}
     </td>
-    <td class="fte-col">{{data.fte}}</td>
+    <td class="fte-col">{{ row.fte }}</td>   <!-- ✅ add class here -->
   </tr>
-  <tr class="total">
-    <td class="text-left">Total</td>
-    <td class="fte-col">{{total}}</td>
+
+  <tr *ngFor="let t of total" class="total">
+    <td class="text-left">{{ t.label }}</td>
+    <td class="fte-col">{{ t.fte }}</td>     <!-- ✅ add class here -->
   </tr>
 </tbody>
 
 
 .table {
   width: 100%;
+  border-collapse: collapse;
 
-  th.fte-col,
-  td.fte-col {
-    text-align: right;   // pushes header + values to the right edge
-    padding-right: 12px; // matches spacing with your design
+  th, td {
+    padding: 8px 12px;
+    font-size: 13px;
+    vertical-align: middle;
   }
 
-  th.text-left,
-  td.text-left {
+  .fte-col {
+    text-align: right !important;   // force alignment
+    padding-right: 20px;
+    width: 100px;                   // optional: give column fixed width so it hugs right edge
+  }
+
+  td:first-child {
+    display: flex;
+    align-items: center;
+    gap: 8px;
     text-align: left;
-    padding-left: 12px;
+    padding-left: 8px;
+  }
+
+  .total {
+    font-weight: 600;
+    background: #f8f8f8;
   }
 }
