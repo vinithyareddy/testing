c725d10f8a5023e847d41e44cb87b51af13877ff
vinithya <vinithyareddyp.com>
2025-08-28 14:01:18 -0400

    477

--- test.ts
+++ test.ts
@@ -1,41 +1,35 @@
-<table class="table">
-  <thead class="tableHeading">
-    <tr>
-      <th class="location-col text-left">Location and Job</th>
-      <th class="fte-col">FTE</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr *ngFor="let data of groupdata">
-      <td class="location-col">
-        <i class="far fa-plus-circle"></i> &nbsp; {{data.country}}
-      </td>
-      <td class="fte-col">{{data.fte}}</td>
-    </tr>
-    <tr class="total">
-      <td class="location-col">Total</td>
-      <td class="fte-col">{{total}}</td>
-    </tr>
-  </tbody>
-</table>
+<thead class="tableHeading">
+  <tr>
+    <th class="text-left">Location and Job</th>
+    <th class="fte-col">FTE</th>
+  </tr>
+</thead>
+<tbody>
+  <tr *ngFor="let data of groupdata">
+    <td class="text-left">
+      <i class="far fa-plus-circle"></i> &nbsp; {{data.country}}
+    </td>
+    <td class="fte-col">{{data.fte}}</td>
+  </tr>
+  <tr class="total">
+    <td class="text-left">Total</td>
+    <td class="fte-col">{{total}}</td>
+  </tr>
+</tbody>
 
 
 .table {
   width: 100%;
-  border-collapse: collapse;
 
-  .location-col {
+  th.fte-col,
+  td.fte-col {
+    text-align: right;   // pushes header + values to the right edge
+    padding-right: 12px; // matches spacing with your design
+  }
+
+  th.text-left,
+  td.text-left {
     text-align: left;
     padding-left: 12px;
-  }
-
-  .fte-col {
-    text-align: right;
-    padding-right: 20px;
-    width: 80px;   // keeps column fixed width
-  }
-
-  .total .fte-col {
-    font-weight: 600;
   }
 }
