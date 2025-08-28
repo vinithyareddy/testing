a0f2d116f4f849fdaedec4d6a2a84014dd2413c7
vinithya <vinithyareddyp.com>
2025-08-27 20:03:43 -0400

    438

--- test.ts
+++ test.ts
@@ -1,17 +1,7 @@
-legend: {
-  enabled: true,
-  layout: 'horizontal',
-  align: 'center',
-  verticalAlign: 'bottom',
-  symbolRadius: 8,     // round dots
-  symbolWidth: 10,
-  symbolHeight: 10,
-  itemDistance: 30,    // spacing between FCV and Non-FCV
-  itemStyle: {
-    color: '#2f2f2f',
-    fontSize: '14px',
-    fontWeight: '400'
-  },
-  padding: 0,
-  margin: 0
-},
+series: [{
+  type: 'pie',
+  data: [
+    { name: 'FCV', y: this.fcv, color: this.colorFCV },
+    { name: 'Non - FCV', y: this.nonFcv, color: this.colorNonFCV }
+  ]
+}]
