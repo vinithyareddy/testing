60e0ac8f45af6ea8f9c2afaf8478df428d3b9e7e
vinithya <vinithyareddyp.com>
2025-08-28 13:57:21 -0400

    475

--- test.ts
+++ test.ts
@@ -1,56 +1,39 @@
+<table class="table">
+  <thead class="tableHeading">
+    <tr>
+      <th class="location-col text-left">Location and Job</th>
+      <th class="fte-col">FTE</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr *ngFor="let data of groupdata">
+      <td class="location-col">
+        <i class="far fa-plus-circle"></i> &nbsp; {{data.country}}
+      </td>
+      <td class="fte-col">{{data.fte}}</td>
+    </tr>
+    <tr class="total">
+      <td class="location-col">Total</td>
+      <td class="fte-col">{{total}}</td>
+    </tr>
+  </tbody>
+</table>
 .table {
   width: 100%;
   border-collapse: collapse;
 
-  th, td {
-    padding: 8px 12px;
-    font-size: 13px;
-    vertical-align: middle;
+  .location-col {
+    text-align: left;
+    padding-left: 12px;
   }
 
-  thead {
-    .tableHeading {
-      font-weight: 600;
-      color: #2d2d2d;
-    }
-
-    th:first-child {
-      text-align: left;
-      padding-left: 8px;   // keep expand icon flush left
-    }
-
-    th:last-child {
-      text-align: right;   // align FTE header to far right
-      padding-right: 16px;
-    }
+  .fte-col {
+    text-align: right;
+    padding-right: 20px;
+    width: 80px;   // keeps column fixed width
   }
 
-  tbody {
-    td:first-child {
-      display: flex;
-      align-items: center;
-      gap: 8px;            // space between icon, flag, and text
-      text-align: left;
-      padding-left: 8px;   // push + icon flush left
-    }
-
-    td:last-child {
-      text-align: right;   // align FTE values to right edge
-      padding-right: 16px;
-    }
-  }
-
-  .total {
+  .total .fte-col {
     font-weight: 600;
-    background: #f8f8f8;
-
-    td:first-child {
-      padding-left: 8px;
-    }
-
-    td:last-child {
-      text-align: right;
-      padding-right: 16px;
-    }
   }
 }
