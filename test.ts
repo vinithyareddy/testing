cf5254763651b71387ea03c54eb7ce38b531687f
vinithya <vinithyareddyp.com>
2025-08-28 13:45:45 -0400

    474

--- test.ts
+++ test.ts
@@ -14,29 +14,29 @@
       color: #2d2d2d;
     }
 
-    th {
-      text-align: left;   // default all headers left
+    th:first-child {
+      text-align: left;
+      padding-left: 8px;   // keep expand icon flush left
     }
 
     th:last-child {
-      text-align: right;  // force FTE header to right
+      text-align: right;   // align FTE header to far right
+      padding-right: 16px;
     }
   }
 
   tbody {
-    td {
-      text-align: left;   // all cells left by default
+    td:first-child {
+      display: flex;
+      align-items: center;
+      gap: 8px;            // space between icon, flag, and text
+      text-align: left;
+      padding-left: 8px;   // push + icon flush left
     }
 
     td:last-child {
-      text-align: right;  // force FTE values right
-    }
-
-    // align expand/collapse icon to extreme left
-    .expand-cell {
-      display: flex;
-      align-items: center;
-      gap: 6px; // space between + icon and country text
+      text-align: right;   // align FTE values to right edge
+      padding-right: 16px;
     }
   }
 
@@ -44,20 +44,13 @@
     font-weight: 600;
     background: #f8f8f8;
 
-    td {
-      text-align: left;
+    td:first-child {
+      padding-left: 8px;
     }
 
     td:last-child {
       text-align: right;
+      padding-right: 16px;
     }
   }
 }
-
-
-<td class="expand-cell pointer text-left" (click)="row.expanded = !row.expanded">
-  <i class="far" [ngClass]="row.expanded ? 'fa-minus-circle' : 'fa-plus-circle'"></i>
-  <img [src]="row.flag" alt="flag" width="18" height="12" />
-  {{ row.country }}
-</td>
-<td>{{ row.fte | number }}</td>
