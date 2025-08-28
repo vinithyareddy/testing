b98fe7bd1e03f0bba49ce08b732930da66b35ad3
vinithya <vinithyareddyp.com>
2025-08-28 12:17:23 -0400

    471

--- test.ts
+++ test.ts
@@ -1,7 +1,9 @@
-import * as Highcharts from 'highcharts';
-import More from 'highcharts/highcharts-more';
-import SolidGauge from 'highcharts/modules/solid-gauge';
-
-// Initialize the modules
-More(Highcharts);
-SolidGauge(Highcharts);
+.ellipsis {
+  width: 28px;
+  height: 28px;
+  border: 1px solid #d6d6d6;
+  text-align: center;
+  line-height: 28px;
+  cursor: pointer;
+  margin-left: 6px; // only ellipsis gets spacing from pie toggle
+}
