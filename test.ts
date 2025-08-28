2f48a3af6acaac58d58356c8b96c850056ee2064
vinithya <vinithyareddyp.com>
2025-08-28 18:31:40 -0400

    519

--- test.ts
+++ test.ts
@@ -1,82 +1,154 @@
-<div [ngClass]="{ 'full-view': fullview }">
-  <div class="budget-card-box">
-    <div class="budget-box-chart">
+.budget-card-box {
+  background: #fff;
+  box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.1);
+  margin-top: 25px;
 
-      <!-- Header -->
-      <div class="row card-box-header-sec align-items-center">
-        <!-- Title + info -->
-        <div class="col-md-9 d-flex align-items-center heading-sec">
-          <span class="title-text">
-            Workforce Supply (FTE) by Country & Job
-          </span>
-          <ng-template [ngTemplateOutlet]="infotemp"></ng-template>
-        </div>
+  /* Title + info icon */
+  .heading-sec {
+    display: flex;
+    align-items: center;
+    gap: 6px;
+    margin-bottom: 12px;
+  }
 
-        <!-- Right icons -->
-        <div class="col-md-3 d-flex justify-content-end align-items-center header-icons">
-          <span (click)="fullPageView()" class="view">
-            <i class="fas fa-expand" title="Zoom"></i>
-          </span>
-          <div class="ellipsis ml-2">
-            <i class="fas fa-ellipsis-v"></i>
-          </div>
-        </div>
-      </div>
+  .title-text {
+    font-size: 16px;
+    font-weight: 600;
+    color: #2d2d2d;
+  }
 
-      <!-- Table -->
-      <div [@collapse]="collapsed">
-        <ng-container *ngIf="!ResponseFlag">
-          <div class="loader-img">
-            <lift-section-loader></lift-section-loader>
-          </div>
-        </ng-container>
+  i.fa-info-circle {
+    font-size: 12px;
+    color: #0071bc;
+    cursor: pointer;
+  }
 
-        <ng-container *ngIf="ResponseFlag">
-          <div class="TableView">
-            <table class="table">
-              <thead>
-                <tr>
-                  <th class="text-left">Location and Job</th>
-                  <th class="fte-col">FTE</th>
-                </tr>
-              </thead>
+  /* Right icons */
+  .header-icons {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    gap: 12px;
 
-              <tbody>
-                <tr *ngFor="let row of groupdata">
-                  <td class="pointer text-left" (click)="expandRow(row)">
-                    <span class="cell-content">
-                      <i *ngIf="!row.expanded" class="far fa-plus-circle"></i>
-                      <i *ngIf="row.expanded" class="far fa-minus-circle"></i>
-                      <img [src]="'https://flagcdn.com/16x12/' + row.flag + '.png'" class="flag-icon" />
-                      {{ row.country }}
-                    </span>
-                  </td>
-                  <td class="fte-col">{{ row.fte }}</td>
-                </tr>
-              </tbody>
+    i {
+      font-size: 16px;
+      cursor: pointer;
+    }
+  }
 
-              <!-- ✅ Total row inside tfoot -->
-              <tfoot>
-                <tr *ngFor="let t of total" class="total">
-                  <td class="text-left">{{ t.label }}</td>
-                  <td class="fte-col">{{ t.fte }}</td>
-                </tr>
-              </tfoot>
-            </table>
-          </div>
+  .ellipsis {
+    cursor: pointer;
+    font-size: 18px;
+    margin-left: 12px;
+    color: #0071bc;
+  }
 
-          <!-- View More -->
-          <div class="viewmore pointer mt-3" (click)="getDetailPage()">
-            <span>View More&nbsp;&nbsp;</span><i class="fa fa-angle-right"></i>
-          </div>
-        </ng-container>
-      </div>
-    </div>
-  </div>
-</div>
+  .view {
+    font-size: 18px;
+    color: #0071bc;
+  }
 
-<ng-template #infotemp>
-  <lift-popover popoverTitle="Workforce Supply (FTE) by Country & Job" popoverText="">
-    <span><i class="far fa-info-circle"></i></span>
-  </lift-popover>
-</ng-template>
+  /* Table section */
+  .TableView {
+    max-height: 335px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    margin-bottom: 20px;
+
+    .table {
+      width: 100%;
+      border-collapse: collapse;
+      table-layout: fixed;
+
+      th,
+      td {
+        padding: 8px 12px;
+        font-size: 13px;
+        vertical-align: middle;
+        white-space: nowrap;
+      }
+
+      /* ✅ Alternating row colors */
+      tbody tr:nth-of-type(odd) {
+        background-color: #fff;
+      }
+
+      tbody tr:nth-of-type(even) {
+        background-color: #f4f6f9;
+      }
+
+      tbody tr:hover {
+        background-color: #eef5ff;
+      }
+
+      th:first-child,
+      td:first-child {
+        width: calc(100% - 100px);
+        text-align: left;
+        padding-left: 12px;
+      }
+
+      th:last-child,
+      td:last-child {
+        width: 100px;
+        text-align: right;
+        padding-right: 20px;
+      }
+
+      thead th {
+        font-weight: 600;
+        color: #2d2d2d;
+        background-color: #f4f6f9;
+        position: sticky;
+        top: 0;
+        z-index: 2;
+      }
+
+      tbody .cell-content i {
+        margin-right: 10px;
+        display: inline-flex;
+        align-items: center;
+        gap: 8px;
+        color: #0071bc;
+      }
+
+      .fte-col {
+        width: 100px;
+        text-align: right !important;
+        padding-right: 20px;
+      }
+
+      /* ✅ Total row always visible at bottom */
+      tfoot tr.total {
+        font-weight: 600;
+        background: #f4f6f9;
+        color: #2d2d2d;
+        position: sticky;
+        bottom: 0;
+        z-index: 2;
+
+        td:first-child {
+          padding-left: 8px;
+        }
+
+        td:last-child {
+          text-align: right;
+          padding-right: 20px;
+        }
+      }
+    }
+  }
+
+  .flag-icon {
+    width: 18px;
+    height: 12px;
+    margin-right: 6px;
+  }
+
+  .viewmore {
+    font-size: 13px;
+    font-weight: 500;
+    color: #0071bc;
+    text-align: right;
+  }
+}
