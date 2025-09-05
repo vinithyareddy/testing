c45bbbea8ea0db8c96fe9cf2c75def844edf4f69
vinithya <vinithyareddyp.com>
2025-09-05 09:38:05 -0400

    595

--- test.ts
+++ test.ts
@@ -1,4 +1,8 @@
+this.globe = new Globe() as any;
 (this.globe as any)
+  .globeImageUrl('//unpkg.com/three-globe/example/img/earth-dark.jpg')
+  .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
+  .pointsData(this.laborData)
   .pointLat('lat')
   .pointLng('lon')
   .pointAltitude(() => 0.05)
