1cdb65321c5c0cf297f196b136e2a45d668add2c
vinithya <vinithyareddyp.com>
2025-09-05 10:44:03 -0400

    596

--- test.ts
+++ test.ts
@@ -1,10 +1,10 @@
 this.globe = new Globe() as any;
 (this.globe as any)
-  .globeImageUrl('//unpkg.com/three-globe/example/img/earth-dark.jpg')
-  .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
+  .globeMaterial(new THREE.MeshPhongMaterial({ color: 0x4da6ff })) // 🌍 light blue
   .pointsData(this.laborData)
   .pointLat('lat')
   .pointLng('lon')
   .pointAltitude(() => 0.05)
-  .pointColor(() => 'orange')
+  .pointColor(() => 'darkblue') // marker color, you can pick red/orange if you like
   .pointLabel((d: any) => `${d.country || d.region}<br/>Cost: $${d.avgCost}`);
+this.scene.add(this.globe);
