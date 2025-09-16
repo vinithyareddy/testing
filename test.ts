5b57cf201731fc2184011954eb123cd4a1c48451
vinithya <vinithyareddyp.com>
2025-09-16 10:04:55 -0400

    972

--- test.ts
+++
@@ -1,8 +0,0 @@
-<div class="map-modes">
-  <button (click)="setMode('satellite')">Satellite</button>
-  <button (click)="setMode('terrain')">Terrain</button>
-  <button (click)="setMode('transit')">Transit</button>
-  <button (click)="setMode('biking')">Biking</button>
-  <button (click)="setMode('night')">Night</button>
-  <button (click)="setMode('light')">Light</button>
-</div>
