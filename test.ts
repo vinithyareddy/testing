213665d6871db7f2e0539118ece428498d7bb435
vinithya <vinithyareddyp.com>
2025-09-17 09:56:49 -0400

    975

---
+++ test.ts
@@ -0,0 +1,15 @@
+npm install @angular/core@17 @angular/common@17 @angular/compiler@17 @angular/forms@17 @angular/platform-browser@17 @angular/platform-browser-dynamic@17 @angular/router@17 --save
+
+
+npm install @angular/cli@17 @angular-devkit/build-angular@17 --save-dev
+
+
+
+
+npm install @angular/animations@17 @angular/service-worker@17 --save
+
+
+npm install ngx-skeleton-loader@11 --save
+
+
+npm install
