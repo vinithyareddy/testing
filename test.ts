1c5d777c1bc73215f3ff49690a3c2300ea04d8fc
vinithya <vinithyareddyp.com>
2025-09-22 14:34:46 -0400

    1094

--- test.ts
+++ test.ts
@@ -1,62 +1,33 @@
-<ng-template #filterTemp>
-  <div class="filter-container full-width" #filterContainer>
-    @if (swfpSelectedFilter().length >= 1 && swfpSelectedFilter()[0].key !== '') {
-      <span class="me-3" style="font-size: 15px"> Filters: </span>
-
-      <!-- Show first 10 or all depending on expand state -->
-      @for (item of (expanded ? swfpSelectedFilter() : swfpSelectedFilter().slice(0, 10)); track item) {
-        <span class="filterCSS">
-          {{item.title}}:{{ item.displayValue }}
-          <span class="ps-1" (click)="removeFilter(item)">
-            <i class="fa fa-times" aria-hidden="true"></i>
-          </span>
-        </span>
-      }
-
-      <!-- View More / Show Less -->
-      @if (swfpSelectedFilter().length > 10) {
-        <span class="resetCSS toggle-btn" (click)="toggleExpand()">
-          {{ expanded ? 'Show Less' : 'View More' }}
-        </span>
-      }
-    }
+<div class="row bar px-3 filters-row">
+  <div class="filters-wrap">
+    <ng-template [ngTemplateOutlet]="filterTemp"></ng-template>
   </div>
-</ng-template>
+
+  <div class="filter-btn">
+    <span class="tagviewShow" (click)="onFilterOpen()">
+      <lift-tag text="Filter" class="tagview" [size]="smallSize" [readOnly]="true" [active]="tagActive"
+        image="../../../../../../../assets/images/sliders.svg">
+      </lift-tag>
+    </span>
+  </div>
+</div>
 
 
-.filter-container {
+.filters-row {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.filters-wrap {
+  flex: 1; /* take all available width */
+  margin-right: 10px;
   display: flex;
   flex-wrap: wrap;
   align-items: center;
-  width: 100%; // take full row width
 }
 
-.filterCSS {
-  display: inline-block;
-  padding: 0 10px;
-  font-size: 12px;
-  line-height: 24px;
-  border-radius: 12px;
-  background-color: #DBE1ED;
-  cursor: pointer;
-  margin: 2px 5px 2px 0;
-
-  i.fa-times {
-    color: rgba(0, 0, 0, 0.6);
-    font-size: 11px;
-  }
+.filter-btn {
+  flex-shrink: 0; /* button stays fixed on the right */
 }
-
-/* Push "View More / Show Less" to the right end */
-.toggle-btn {
-  margin-left: auto;
-  white-space: nowrap;
-}
-
-.resetCSS {
-  color: #0b50d1;
-  font-weight: 600;
-  font-size: 14px;
-  margin: 2px;
-  cursor: pointer;
-}
+1094
