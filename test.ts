f804a0010f143d5410e2a75162cdb13b871f951b
vinithya <vinithyareddyp.com>
2025-09-24 15:54:33 -0400

    1194

--- test.ts
+++ test.ts
@@ -1,15 +1,15 @@
 this.svg.selectAll('.ocean-label')
   .attr('x', (d: any, i: number) => {
-    const coords = this.oceans.features[i].geometry.coordinates;
+    const coords = (this.oceans.features[i].geometry as any).coordinates as [number, number];
     const projected = this.projection(coords);
     return projected ? projected[0] : 0;
   })
   .attr('y', (d: any, i: number) => {
-    const coords = this.oceans.features[i].geometry.coordinates;
+    const coords = (this.oceans.features[i].geometry as any).coordinates as [number, number];
     const projected = this.projection(coords);
     return projected ? projected[1] : 0;
   })
   .style('opacity', (d: any, i: number) => {
-    const coords = this.oceans.features[i].geometry.coordinates;
-    return this.isPointVisible([coords[0], coords[1]]) ? 0.7 : 0;
+    const coords = (this.oceans.features[i].geometry as any).coordinates as [number, number];
+    return this.isPointVisible(coords) ? 0.7 : 0;
   });
