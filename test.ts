05cfd64475d79c1d3abfa90b9623de5e51cbdcda
vinithya <vinithyareddyp.com>
2025-09-25 11:20:35 -0400

    1224

--- test.ts
+++ test.ts
@@ -1,34 +1,8 @@
-private drawCountries() {
-  this.svg.selectAll('.country').remove();
-
-  this.svg.selectAll('.country')
-    .data(this.countries.features)
-    .enter()
-    .append('path')
-    .attr('class', 'country')
-    .attr('d', this.path)
-    .attr('fill', (d: any) => this.getCountryColor(d))
-    .attr('stroke', STROKE_COLOR_COUNTRY)
-    .attr('stroke-width', 0.5);
-
-  // Hover layer (optional)
-  this.svg.selectAll('.country-hover')
-    .data(this.countries.features)
-    .enter()
-    .append('path')
-    .attr('class', 'country-hover')
-    .attr('d', this.path)
-    .attr('fill', 'transparent')
-    .style('cursor', 'pointer')
-    .on('mouseover', (event: any, d: any) => {
-      this.isRotating = false;
-      this.showTooltip(event, d);
-    })
-    .on('mousemove', (event: any) => this.moveTooltip(event))
-    .on('mouseout', () => {
-      if (!this.isDragging) this.isRotating = true;
-      this.hideTooltip();
-    });
-
-  this.updateCountryLabels();
-}
+this.svg.append('image')
+  .attr('href', 'assets/images/transparent-globe.png')
+  .attr('x', width / 2 - this.currentRadius)
+  .attr('y', height / 2 - this.currentRadius)
+  .attr('width', this.currentRadius * 2)
+  .attr('height', this.currentRadius * 2)
+  .style('pointer-events', 'none') // so zoom/drag still work
+  .style('opacity', 0.9); // adjust transparency
