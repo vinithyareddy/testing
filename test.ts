b9d72818bb5f05b51ba7e47638c887dddf9139f1
vinithya <vinithyareddyp.com>
2025-09-25 10:12:58 -0400

    1211

--- test.ts
+++ test.ts
@@ -1,25 +1,64 @@
-// Base teal ocean
+// Base teal ocean circle
 this.svg.append('circle')
   .attr('cx', width / 2)
   .attr('cy', height / 2)
   .attr('r', this.currentRadius)
-  .attr('fill', GLOBE_TEAL_COLOR);
+  .attr('fill', CUSTOM_GLOBE_COLOR)
+  .attr('stroke', '#ccc')
+  .attr('stroke-width', 1)
+  .style('filter', 'url(#glow)');
 
-// Create mask from PNG (white = ocean will be transparent)
-const defs = this.svg.append('defs');
-defs.append('mask')
-  .attr('id', 'land-mask')
-  .append('image')
-  .attr('href', 'assets/images/globe-texture.png')
+// Overlay transparent globe texture (land only)
+this.svg.append('image')
+  .attr('href', 'assets/images/transparent-globe.png')
+  .attr('x', width / 2 - this.currentRadius)
+  .attr('y', height / 2 - this.currentRadius)
   .attr('width', this.currentRadius * 2)
   .attr('height', this.currentRadius * 2)
-  .attr('x', 0)
-  .attr('y', 0);
+  .attr('preserveAspectRatio', 'xMidYMid slice')
+  .attr('clip-path', `circle(${this.currentRadius}px at ${width/2}px ${height/2}px)`);
 
-// Apply the mask on top of teal circle
-this.svg.append('circle')
-  .attr('cx', width / 2)
-  .attr('cy', height / 2)
-  .attr('r', this.currentRadius)
-  .attr('fill', 'black')   // mask will keep only land visible
-  .attr('mask', 'url(#land-mask)');
+
+  private drawCountries() {
+    // remove old filled countries
+    this.svg.selectAll('.country').remove();
+  
+    // Add invisible paths just for interaction (tooltip/highlight)
+    this.svg.selectAll('.country-hover')
+      .data(this.countries.features)
+      .enter()
+      .append('path')
+      .attr('class', 'country-hover')
+      .attr('d', this.path)
+      .attr('fill', 'transparent')
+      .attr('stroke', 'none')
+      .style('cursor', 'pointer')
+      .on('mouseover', (event: any, d: any) => {
+        this.isRotating = false;
+        this.showTooltip(event, d);
+      })
+      .on('mousemove', (event: any) => this.moveTooltip(event))
+      .on('mouseout', () => {
+        if (!this.isDragging) {
+          this.isRotating = true;
+        }
+        this.hideTooltip();
+      });
+  
+    this.updateCountryLabels();
+  }
+
+  
+  // update teal circle
+this.svg.select('circle')
+.attr('cx', width / 2)
+.attr('cy', height / 2)
+.attr('r', this.currentRadius * this.currentZoom);
+
+// update overlay image
+this.svg.select('image')
+.attr('x', width/2 - this.currentRadius * this.currentZoom)
+.attr('y', height/2 - this.currentRadius * this.currentZoom)
+.attr('width', this.currentRadius * 2 * this.currentZoom)
+.attr('height', this.currentRadius * 2 * this.currentZoom)
+.attr('clip-path', `circle(${this.currentRadius * this.currentZoom}px at ${width/2}px ${height/2}px)`);
