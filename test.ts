e1fc6a22ee74cce445da2e6d171d4d8fb4b3bd2f
vinithya <vinithyareddyp.com>
2025-09-25 11:12:45 -0400

    1223

--- test.ts
+++ test.ts
@@ -1,14 +1,34 @@
-private startRotation() {
-  const rotate = () => {
-    if (this.isRotating && !this.isDragging) {
-      this.currentRotation[0] += ROTATION_SPEED;
-      this.projection.rotate(this.currentRotation);
+private drawCountries() {
+  this.svg.selectAll('.country').remove();
 
-      // Re-render everything based on new projection
-      this.updateCountries();
-      this.updateStates();
-    }
-    requestAnimationFrame(rotate);
-  };
-  rotate();
+  this.svg.selectAll('.country')
+    .data(this.countries.features)
+    .enter()
+    .append('path')
+    .attr('class', 'country')
+    .attr('d', this.path)
+    .attr('fill', (d: any) => this.getCountryColor(d))
+    .attr('stroke', STROKE_COLOR_COUNTRY)
+    .attr('stroke-width', 0.5);
+
+  // Hover layer (optional)
+  this.svg.selectAll('.country-hover')
+    .data(this.countries.features)
+    .enter()
+    .append('path')
+    .attr('class', 'country-hover')
+    .attr('d', this.path)
+    .attr('fill', 'transparent')
+    .style('cursor', 'pointer')
+    .on('mouseover', (event: any, d: any) => {
+      this.isRotating = false;
+      this.showTooltip(event, d);
+    })
+    .on('mousemove', (event: any) => this.moveTooltip(event))
+    .on('mouseout', () => {
+      if (!this.isDragging) this.isRotating = true;
+      this.hideTooltip();
+    });
+
+  this.updateCountryLabels();
 }
