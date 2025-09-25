82ebf4aa36cee550e4f570376625c841b0c95cbf
vinithya <vinithyareddyp.com>
2025-09-25 10:04:26 -0400

    1209

--- test.ts
+++ test.ts
@@ -1,7 +1,31 @@
-private updateTextureRotation() {
-  const rotation = this.currentRotation[0]; // longitude rotation
-  const tilt = this.currentRotation[1];     // latitude rotation
+// Base teal ocean
+this.svg.append('circle')
+  .attr('cx', width / 2)
+  .attr('cy', height / 2)
+  .attr('r', this.currentRadius)
+  .attr('fill', GLOBE_TEAL_COLOR)
+  .attr('stroke', '#ccc')
+  .attr('stroke-width', 1);
 
-  this.svg.select('#globe-texture')
-    .attr('patternTransform', `rotate(${rotation}, ${this.currentRadius}, ${this.currentRadius})`);
-}
+// Define pattern for land texture
+const defs = this.svg.append('defs');
+defs.append('pattern')
+  .attr('id', 'globe-texture')
+  .attr('patternUnits', 'objectBoundingBox')
+  .attr('width', 1)
+  .attr('height', 1)
+  .append('image')
+  .attr('href', 'assets/images/globe-texture.png')
+  .attr('width', this.currentRadius * 2)
+  .attr('height', this.currentRadius * 2)
+  .attr('x', 0)
+  .attr('y', 0);
+
+// Overlay continents (fixed on top of teal base)
+this.svg.append('circle')
+  .attr('cx', width / 2)
+  .attr('cy', height / 2)
+  .attr('r', this.currentRadius)
+  .attr('fill', 'url(#globe-texture)')
+  .attr('stroke', '#ccc')
+  .attr('stroke-width', 1);
