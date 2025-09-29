b3498e1aadcbac7a4b00e09e799e91c4c7a4d198
vinithya <vinithyareddyp.com>
2025-09-29 11:00:18 -0400

    1247

--- test.ts
+++ test.ts
@@ -6,3 +6,32 @@
         <i class="fa fa-bar-chart fnticon" aria-hidden="true"></i>
     </div>
 </div>
+
+
+
+
+<div style="flex-grow: 1;">
+    <ng-container class="chart-section">
+        <!-- Chart View -->
+        <highcharts-chart *ngIf="viewMode === 'chart'"
+            [Highcharts]="Highcharts" [options]="chartOptions"
+            style="width: 99%; height: 330px; display: block;">
+        </highcharts-chart>
+
+        <!-- Table View -->
+        <table *ngIf="viewMode === 'table'" class="table table-bordered table-striped mt-2">
+            <thead>
+                <tr>
+                    <th>Proficiency Level</th>
+                    <th *ngFor="let name of ['Awareness','Skilled','Advanced','Expert']">{{name}}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr *ngFor="let category of allCategories; let i = index">
+                    <td>{{category}}</td>
+                    <td *ngFor="let series of allSeriesData">{{series[i]}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </ng-container>
+</div>
