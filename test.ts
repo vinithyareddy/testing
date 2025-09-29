baa18d1c1c43cd40e615febc62d1b6ff61bf9d5e
vinithya <vinithyareddyp.com>
2025-09-29 11:09:12 -0400

    1250

--- test.ts
+++ test.ts
@@ -1,47 +1,38 @@
-<!-- Chart mode stays inside the styled box -->
-<div class="inner-chart-box" *ngIf="viewMode === 'chart'">
+<!-- Chart in box -->
+<div class="inner-card-box-lg mt-4" *ngIf="viewMode === 'chart'">
+  <div class="inner-chart-box">
     <span class="prevButton">
-        <a><i class="fa-solid fa-angle-left"></i></a>
+      <a><i class="fa-solid fa-angle-left"></i></a>
     </span>
 
     <div style="flex-grow: 1;">
-        <ng-container class="chart-section">
-            <highcharts-chart
-                [Highcharts]="Highcharts" [options]="chartOptions"
-                style="width: 99%; height: 330px; display: block;">
-            </highcharts-chart>
-        </ng-container>
+      <ng-container class="chart-section">
+        <highcharts-chart
+          [Highcharts]="Highcharts"
+          [options]="chartOptions"
+          style="width: 99%; height: 330px; display: block;">
+        </highcharts-chart>
+      </ng-container>
     </div>
 
     <span>
-        <a class="nextButton"><i class="fa-solid fa-angle-right"></i></a>
+      <a class="nextButton"><i class="fa-solid fa-angle-right"></i></a>
     </span>
+  </div>
 </div>
 
-<!-- Table mode OUTSIDE the chart box -->
+<!-- Table OUTSIDE the box -->
 <table *ngIf="viewMode === 'table'" class="table table-bordered table-striped mt-3 w-100">
-    <thead>
-        <tr>
-            <th>Proficiency Level</th>
-            <th *ngFor="let name of ['Awareness','Skilled','Advanced','Expert']">{{name}}</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr *ngFor="let category of allCategories; let i = index">
-            <td>{{category}}</td>
-            <td *ngFor="let series of allSeriesData">{{series[i]}}</td>
-        </tr>
-    </tbody>
+  <thead>
+    <tr>
+      <th>Proficiency Level</th>
+      <th *ngFor="let name of ['Awareness','Skilled','Advanced','Expert']">{{name}}</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr *ngFor="let category of allCategories; let i = index">
+      <td>{{category}}</td>
+      <td *ngFor="let series of allSeriesData">{{series[i]}}</td>
+    </tr>
+  </tbody>
 </table>
-
-
-
-table {
-  width: 100%;
-  border-collapse: collapse;
-
-  th, td {
-    text-align: center;
-    padding: 6px 10px;
-  }
-}
