f1a201deb773ff12d70e7b142fe13352a76e95d8
vinithya <vinithyareddyp.com>
2025-09-29 11:04:11 -0400

    1248

--- test.ts
+++ test.ts
@@ -1,37 +1,7 @@
-<div class="togglebtn d-flex">
-    <div class="lft-toggle" [class.lft-toggle-active]="viewMode === 'table'" (click)="viewMode = 'table'">
-        <i class="fa fa-table fnticon" aria-hidden="true"></i>
-    </div>
-    <div class="rgt-toggle" [class.rgt-toggle-active]="viewMode === 'chart'" (click)="viewMode = 'chart'">
-        <i class="fa fa-bar-chart fnticon" aria-hidden="true"></i>
-    </div>
-</div>
+<span class="prevButton" *ngIf="viewMode === 'chart'">
+    <a><i class="fa-solid fa-angle-left"></i></a>
+</span>
 
-
-
-
-<div style="flex-grow: 1;">
-    <ng-container class="chart-section">
-        <!-- Chart View -->
-        <highcharts-chart *ngIf="viewMode === 'chart'"
-            [Highcharts]="Highcharts" [options]="chartOptions"
-            style="width: 99%; height: 330px; display: block;">
-        </highcharts-chart>
-
-        <!-- Table View -->
-        <table *ngIf="viewMode === 'table'" class="table table-bordered table-striped mt-2">
-            <thead>
-                <tr>
-                    <th>Proficiency Level</th>
-                    <th *ngFor="let name of ['Awareness','Skilled','Advanced','Expert']">{{name}}</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr *ngFor="let category of allCategories; let i = index">
-                    <td>{{category}}</td>
-                    <td *ngFor="let series of allSeriesData">{{series[i]}}</td>
-                </tr>
-            </tbody>
-        </table>
-    </ng-container>
-</div>
+<span *ngIf="viewMode === 'chart'">
+    <a class="nextButton"><i class="fa-solid fa-angle-right"></i></a>
+</span>
