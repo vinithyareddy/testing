27d489aa97b3b3b40225937732e11557885523c1
vinithya <vinithyareddyp.com>
2025-09-29 10:59:47 -0400

    1246

--- test.ts
+++ test.ts
@@ -1,5 +1,8 @@
-(globeDiv.querySelectorAll('canvas') as NodeListOf<HTMLCanvasElement>)
-  .forEach((c: HTMLCanvasElement) => c.remove());
-
-(globeDiv.querySelectorAll('svg') as NodeListOf<SVGSVGElement>)
-  .forEach((s: SVGSVGElement) => s.remove());
+<div class="togglebtn d-flex">
+    <div class="lft-toggle" [class.lft-toggle-active]="viewMode === 'table'" (click)="viewMode = 'table'">
+        <i class="fa fa-table fnticon" aria-hidden="true"></i>
+    </div>
+    <div class="rgt-toggle" [class.rgt-toggle-active]="viewMode === 'chart'" (click)="viewMode = 'chart'">
+        <i class="fa fa-bar-chart fnticon" aria-hidden="true"></i>
+    </div>
+</div>
