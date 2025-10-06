7d6e3a8968136ba9a95ba0c096e7f77c1c50da77
vinithya <vinithyareddyp.com>
2025-10-06 12:43:48 -0400

    1266

--- test.ts
+++ test.ts
@@ -1,7 +1,49 @@
-formatter: function () {
-  const xLabel = String(this.x).replace('<br>', ' ');
-  return `<b>${xLabel}</b><br/>` +
-    this.points?.map((p: any) =>
-      `${p.series.name}: ${p.y} (${p.percentage?.toFixed(0)}%)`
-    ).join('<br/>');
-}
+xAxis: {
+  categories: [
+    {
+      name: '2022',
+      categories: ['Planned', 'Actuals']
+    },
+    {
+      name: '2023',
+      categories: ['Planned', 'Actuals']
+    },
+    {
+      name: '2024',
+      categories: ['Planned', 'Actuals']
+    },
+    {
+      name: '2025',
+      categories: ['Planned', 'Actuals']
+    }
+  ],
+  labels: {
+    style: { fontSize: '12px', color: '#333' }
+  }
+},
+
+
+{
+  name: 'Buy',
+  type: 'column',
+  color: '#4A90E2',
+  yAxis: 1,
+  data: [3, 2, 3, 3, 3, 3, 3, 3],
+  stack: 'PlannedActuals'
+},
+{
+  name: 'Build',
+  type: 'column',
+  color: '#7B68EE',
+  yAxis: 1,
+  data: [9, 6, 6, 6, 6, 6, 5, 5],
+  stack: 'PlannedActuals'
+},
+{
+  name: 'Borrow',
+  type: 'column',
+  color: '#5CC4A2',
+  yAxis: 1,
+  data: [18, 17, 13, 13, 12, 12, 11, 11],
+  stack: 'PlannedActuals'
+},
