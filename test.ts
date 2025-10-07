c4804b33607d450b487c57d821602a9c402d8a57
vinithya <vinithyareddyp.com>
2025-10-07 09:35:53 -0400

    1291

--- test.ts
+++ test.ts
@@ -1,31 +1,7 @@
-.togglebtn {
-  display: flex;
+.fa-chart-line-up {
+  display: inline-flex;
   align-items: center;
   justify-content: center;
-
-  div {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 36px;
-    height: 36px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    margin-right: 4px;
-    cursor: pointer;
-
-    &.lft-toggle-active,
-    &.rgt-toggle-active {
-      background-color: #0071bc;
-      i {
-        color: #fff;
-      }
-    }
-
-    i {
-      font-size: 16px;
-      color: #0071bc;
-      line-height: 1; // ensures proper vertical centering
-    }
-  }
+  vertical-align: middle;
+  transform: translateY(1px); /* adjust +1 or -1 as needed */
 }
