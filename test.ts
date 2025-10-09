2b83051c1cf6ed34ab2c1f93075dcad912188b14
vinithya <vinithyareddyp.com>
2025-10-09 11:59:15 -0400

    1354

--- test.ts
+++ test.ts
@@ -1,31 +1,38 @@
-onLeftClick() {
-    if (!this.isLeftDisabled) {
-      this.currentPage--;
-      this.updateChart();
-    }
+<span class="prevButton"
+      [class.disabled]="isLeftDisabled"
+      (click)="onLeftClick()">
+  <i class="fa-solid fa-angle-left"></i>
+</span>
+
+<ng-container class="chart-section">
+  <highcharts-chart
+    [Highcharts]="Highcharts"
+    [options]="chartOptions"
+    style="width: 99%; height: 380px; display: block;">
+  </highcharts-chart>
+</ng-container>
+
+<span class="nextButton"
+      [class.disabled]="isRightDisabled"
+      (click)="onRightClick()">
+  <i class="fa-solid fa-angle-right"></i>
+</span>
+
+
+.prevButton,
+.nextButton {
+  cursor: pointer;
+  font-size: 20px;
+  color: #374151; // dark gray
+  transition: 0.2s;
+
+  &:hover {
+    color: #111827; // darker on hover
   }
-  
-  onRightClick() {
-    if (!this.isRightDisabled) {
-      this.currentPage++;
-      this.updateChart();
-    }
-  }
 
-  
-  <div class="pagination-arrows">
-  <button (click)="onLeftClick()" [disabled]="isLeftDisabled" class="arrow-btn">
-    ←
-  </button>
-
-  <button (click)="onRightClick()" [disabled]="isRightDisabled" class="arrow-btn">
-    →
-  </button>
-</div>
-
-.disabled {
+  &.disabled {
     opacity: 0.4;
     pointer-events: none;
     cursor: not-allowed;
   }
-  
+}
