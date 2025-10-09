b219617d449bd39ad3b995167b5583df2f94c73e
vinithya <vinithyareddyp.com>
2025-10-09 11:54:47 -0400

    1353

--- test.ts
+++ test.ts
@@ -1,30 +1,31 @@
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { Store } from '@ngrx/store';
-import { SwfpFilterState } from 'app/core/swfp-filter-state/swfp-filter-state';
-import { selectEncodedFilter } from 'app/core/swfp-filter-state/swfp-filters.selectors';
-import { SwfpApiService } from 'app/modules/shared/swfp-shared/services/swfp-api.service';
-import { SwfpQueryBuilderService } from 'app/modules/shared/swfp-shared/services/swfp-query-builder.service';
-import { SwfpFilterService } from 'app/modules/shared/swfp-shared/services/swfp-filter.service';
-import { SwfpModuleEnum } from 'app/enums/swfp-module.enum';
-import * as _ from 'lodash';
-import { debounceTime, distinctUntilChanged } from 'rxjs';
-import { DestroyRef, inject } from '@angular/core';
+onLeftClick() {
+    if (!this.isLeftDisabled) {
+      this.currentPage--;
+      this.updateChart();
+    }
+  }
+  
+  onRightClick() {
+    if (!this.isRightDisabled) {
+      this.currentPage++;
+      this.updateChart();
+    }
+  }
 
+  
+  <div class="pagination-arrows">
+  <button (click)="onLeftClick()" [disabled]="isLeftDisabled" class="arrow-btn">
+    ←
+  </button>
 
+  <button (click)="onRightClick()" [disabled]="isRightDisabled" class="arrow-btn">
+    →
+  </button>
+</div>
 
-widgetId: string = 'SSSI_2'; // unique ID for Skill Supply by Location
-module: string = SwfpModuleEnum.HC_WF;
-public fiterDataFromUrl$ = this.store.select(selectEncodedFilter);
-private destroyRef = inject(DestroyRef);
-
-
-
-
-
-constructor(
-    public store: Store<SwfpFilterState>,
-    public queryBuilder: SwfpQueryBuilderService,
-    public filterService: SwfpFilterService,
-    public apiService: SwfpApiService
-  ) { }
+.disabled {
+    opacity: 0.4;
+    pointer-events: none;
+    cursor: not-allowed;
+  }
   
