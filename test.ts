cd5366c3400ea3c56a6b03c41d3f023d28066b4f
vinithya <vinithyareddyp.com>
2025-10-09 15:46:05 -0400

    1360

--- test.ts
+++ test.ts
@@ -1,34 +1,8 @@
-ngOnInit() {
-  this.fiterDataFromUrl$.pipe(
-    distinctUntilChanged((prev, curr) => _.isEqual(prev, curr)),
-    debounceTime(100),
-    takeUntilDestroyed(this.destroyRef)
-  ).subscribe((x: string) => {
-    console.log("filters", x);
-    
-    // Call API and handle response
-    this.apiService.getWidgetData(this.widgetId).subscribe((response) => {
-      console.log("API Response => ", response);
-      
-      // Check if response is an array with data
-      if (response && Array.isArray(response) && response.length > 0) {
-        this.processSkillProficiencyData(response);
-      } else if (response && (response as any).data && Array.isArray((response as any).data) && (response as any).data.length > 0) {
-        // Alternative: if API wraps data in a 'data' property
-        this.processSkillProficiencyData((response as any).data);
-      } else {
-        // No data found - show empty state
-        console.log("No data received from API");
-        this.allCategories = [];
-        this.allSeriesData = [[], [], [], []];
-        this.updateChart();
-      }
-    }, (error) => {
-      console.error("API Error:", error);
-      // Handle error
-      this.allCategories = [];
-      this.allSeriesData = [[], [], [], []];
-      this.updateChart();
-    });
-  });
-}
+select sess.fiscal_year,        
+       sess.skill_name,        
+       sess.prof_skill_overall_name as proficiency,
+       sum(sess.salaried_ind) as fte 
+from dm_hr_gold.v_supply_emp_skills_swfp sess 
+where lower(left(sess.grade,1)) <> 'e' 
+group by sess.fiscal_year, sess.skill_name, sess.prof_skill_overall_name 
+order by 1 desc, 2 asc;
