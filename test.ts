4c41f82ec944de9d65744ccf8e884bab52e64ca1
vinithya <vinithyareddyp.com>
2025-10-16 11:28:00 -0500

    fws

--- test.ts
+++ test.ts
@@ -12,10 +12,13 @@ legend: {
       color: '#051223'
     },
     labelFormatter: function () {
+      // ✅ Add the prefix before the FIRST legend bullet instead of after
       if (this.index === 0) {
-        return '<span style="font-weight:600;color:#051223;margin-right:6px;">Proficiency Level:</span>' + this.name;
+        return '<span style="font-weight:600;color:#051223;margin-right:6px;">Proficiency Level:</span>' +
+               '<span style="vertical-align:middle;">&#9679;&nbsp;' + this.name + '</span>';
       }
-      return this.name;
+      // For all other legend items, show normal bullet and name
+      return '&#9679;&nbsp;' + this.name;
     },
     events: {
       itemClick: function (event: any) {
