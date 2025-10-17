72ab9dcd32f3e95493510b6f04aba940328fec13
vinithya <vinithyareddyp.com>
2025-10-16 21:59:10 -0500

    1409

--- test.ts
+++ test.ts
@@ -1,3 +1,11 @@
-const coords: [number, number] = o.coords as [number, number];
+.attr('d', (d: any) => this.path(d as d3.GeoPermissibleObjects)!)
+
+
+const equator: GeoJSON.LineString = {
+    type: 'LineString',
+    coordinates: d3.range(-180, 181, 1).map((lon): [number, number] => [lon, 0])
+  };
+
+  
+  const coords: [number, number] = o.coords as [number, number];
 const p = this.projection(coords);
-if (!p || !this.isPointVisible(coords)) return;
