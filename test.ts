6730282ac42af806cc164928eda68dfb169497fe
vinithya <vinithyareddyp.com>
2025-10-18 09:33:56 -0500

    1471

--- test.ts
+++ test.ts
@@ -1,18 +1,16 @@
-:host ::ng-deep .layout-wrapper,
-:host ::ng-deep .site-info,
-:host ::ng-deep .container,
-:host ::ng-deep .col-md {
+:host ::ng-deep .layout-wrapper {
+  max-width: 100% !important;
   padding: 0 !important;
   margin: 0 !important;
-  max-width: 100% !important;
   width: 100vw !important;
+  background-color: #f4f7fa !important;
 }
 
-:host {
-  display: block;
-  width: 100vw;
-  min-height: 100vh;
-  background-color: #f4f7fa;
-  margin: 0;
-  padding: 0;
+
+:host ::ng-deep .content-wrapper .layout-wrapper {
+  max-width: 100% !important;
+  padding: 0 !important;
+  margin: 0 !important;
+  width: 100vw !important;
+  background-color: #f4f7fa !important;
 }
