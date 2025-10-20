151596f5c56010edfdab1845ab48244922b73a8d
vinithya <vinithyareddyp.com>
2025-10-20 13:39:03 -0500

    1473

--- test.ts
+++ test.ts
@@ -1,19 +1,6 @@
-toggleMenuExpand(menuList: any[], key: string) {
-  menuList.forEach(item => {
-    if (item.key === key) {
-      item.expanded = !item.expanded;
-      item.settings.collapsed = !item.expanded;
-    } else {
-      item.expanded = false;
-      item.settings.collapsed = true;
-    }
-  });
+menuList: any[] = [];
+
+onToggle(key: string) {
+  this.srServicesService.toggleMenuExpand(this.menuList, key);
+  this.fwService.apiSetLeftNavModel(this.menuList);
 }
-
-
-// Ensure both menus start collapsed
-tempmenulist.forEach(m => {
-  m.expanded = false;
-  if (m.settings) m.settings.collapsed = true;
-});
-this.fwService.apiSetLeftNavModel(tempmenulist);
