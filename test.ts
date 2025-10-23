36b61999b5290da48a7e710221f84e82f4d04e25
vinithya <vinithyareddyp.com>
2025-10-23 10:38:38 -0400

    q

--- test.ts
+++ test.ts
@@ -1,4 +1,49 @@
-<div class="search-bar">
-<input type="text" [(ngModel)]="searchQuery" placeholder="Start Typing..." (keyup.enter)="onSearchEnter()" />
-<img src="assets/images/aichat/ai-chat.png" (click)="onSearchEnter()">
+<div class="col-lg-3 col-md-3 pe-0 printhide" style="text-align:end;">
+    @if(pageTitle !== 'AI Search Results' && pageTitle !== 'Prompt Library'){
+    @if (isFilterVisible()) {
+    <span class="tagviewShow" (click)="refinerOpen()">
+        <div class="">
+            <lift-tag text="Filter" [size]="smallSize" [readOnly]="true" [active]="tagActive"
+                image="../../assets/images/sliders.svg">
+            </lift-tag>
+        </div>
+    </span>
+    }
+    @if (!isFilterVisible()) {
+    <span class="tagviewShow disabled" title="Filter option is not available">
+        <lift-tag text="Filter" [disabled]="true" [size]="smallSize" [readOnly]="true"
+            image="../../assets/images/sliders.svg">
+        </lift-tag>
+    </span>
+    }
+    }
 </div>
+
+@if(pageTitle !== 'AI Search Results' && pageTitle !== 'Prompt Library'){
+  <div class="row date-row-height">
+      <div class="col-lg-3 col-md-3 ps-0">
+          @if (pageTitle === 'Overview') {
+          <div class="col-md-12 col-lg-12 text-left ps-0 pt-2">
+              <div class="ps-0  d-inline-block"><span class="fnt-md align-text-middle">{{dataasofdate}}</span>
+              </div>
+          </div>
+          }
+          @if (pageTitle != 'Overview') {
+          <div class="col-md-12 col-lg-12 text-left ps-0 pt-2">
+              <div class="d-inline-block">
+                  <span class="fnt-md align-text-middle">{{dataasofdate}}</span>
+              </div>
+          </div>
+          }
+      </div>
+      <div class="col-lg-9 col-md-9 pe-0 d-inline-block bgt-text-end printhide"
+          style="align-self: flex-end;text-align:end;">
+          @if (isDownloadVisible()) {
+          <button type="button" class="btn btn-outline-primary enabled"
+              styleSheetFile="assets/css/tfprintbootstap.min.css, assets/css/trust-funds-print.css"
+              printSectionId="print-section" [printTitle]="pageTitle" ngxPrint><img
+                  src="assets/images/Download.png">&nbsp;&nbsp; Download</button>
+          }
+      </div>
+  </div>
+  }
