8a9d4ab3d553f1e09f76e7cfb634869f392fd573
vinithya <vinithyareddyp.com>
2025-10-30 10:49:30 -0400

    w

--- test.ts
+++ test.ts
@@ -1,33 +1,28 @@
+<div class="searchBox">
+  <i class="fa fa-search search-icon"></i>
+  <lift-search [data]="data" [config]="config"></lift-search>
+</div>
+
 .searchBox {
-    position: relative;
+    position: relative; /* Important for absolute positioning of icon */
     width: 328px;
     height: 40px;
     color: #fff;
     border-radius: 8px;
-  
-    // make sure the lift-search box fills correctly
-    lift-search {
-      display: block;
-      width: 100%;
-    }
-  
-    // add the search icon
-    &::before {
-      content: "\f002"; // FontAwesome search icon
-      font-family: "Font Awesome 5 Free"; // make sure font awesome is loaded
-      font-weight: 900;
-      position: absolute;
-      left: 12px;
-      top: 50%;
-      transform: translateY(-50%);
-      color: #888;
-      font-size: 14px;
-      pointer-events: none; // prevents blocking input clicks
-    }
-  
-    // make space for the icon inside the input field
-    input {
-      padding-left: 35px !important;
-    }
   }
   
+  .search-icon {
+    position: absolute;
+    left: 12px; /* Adjust distance from left edge */
+    top: 50%;
+    transform: translateY(-50%); /* Centers icon vertically */
+    color: #999; /* Icon color */
+    font-size: 16px;
+    pointer-events: none; /* Allows clicking through icon to input */
+    z-index: 1;
+  }
+  
+  /* Add padding to the actual input inside lift-search */
+  .searchBox ::ng-deep input {
+    padding-left: 35px; /* Make room for the icon */
+  }
