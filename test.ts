6e25947acbbeae836f1059bdea5e8c2ee6744b63
vinithya <vinithyareddyp.com>
2025-10-30 11:47:38 -0400

    f

--- test.ts
+++ test.ts
@@ -9,14 +9,22 @@
     left: 12px;
     top: 50%;
     transform: translateY(-50%);
-    color: #888;
+    color: #666;
     font-size: 16px;
     pointer-events: none;
     z-index: 10;
   }
   
-  /* Target the input with its actual classes */
-  ::ng-deep .searchBox input.placeholder-text,
+  /* Remove any wrapper styling from lift-search */
+  ::ng-deep .searchBox lift-search {
+    display: block !important;
+    width: 100% !important;
+    height: 100% !important;
+    border: none !important;
+    background: transparent !important;
+  }
+  
+  /* Style the actual input */
   ::ng-deep .searchBox input {
     width: 328px !important;
     height: 40px !important;
@@ -27,9 +35,16 @@
     font-size: 14px !important;
     box-sizing: border-box !important;
     background-color: #fff !important;
+    margin: 0 !important;
   }
   
   ::ng-deep .searchBox input::placeholder {
     color: #999 !important;
     font-size: 14px !important;
+  }
+  
+  /* Hide any built-in icon from lift-search if it exists */
+  ::ng-deep .searchBox lift-search .search-icon,
+  ::ng-deep .searchBox lift-search i {
+    display: none !important;
   }
